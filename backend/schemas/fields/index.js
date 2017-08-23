60c886bb6ebbfcf34267d6b026e4cde8840d784c
kmelve <knut@netliferesearch.com>
2017-08-23 10:56:03 +0200

    fix longTitle

--- backend/schemas/fields/index.js
+++ backend/schemas/fields/index.js
@@ -1,14 +1,16 @@
 import title from './title';
-import longTitle from './title';
+import longTitle from './longTitle';
 import explainerText from './explainerText';
 import featuredImage from './featuredImage'
 import date from './date'
+import image from './image';
 
 export { title };
 export { longTitle };
 export { explainerText };
 export { featuredImage };
 export { date };
+export { image };
 
 export default [
   title,
@@ -16,5 +18,6 @@ export default [
   explainerText,
   featuredImage,
   date,
+  image,
 ];
 
