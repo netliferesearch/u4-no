757c304809f24904683ab01020d2ed0b1502d80e
kmelve <knut@netliferesearch.com>
2017-08-23 14:51:44 +0200

    add blurb type

---
+++ backend/schemas/blurb.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'blurb',
+  title: 'Blurb',
+  type: 'object',
+  description: '',
+  fields: [
+    {
+      name: 'text',
+      type: 'text'
+    },
+    {
+      name: 'origin',
+      description: 'Who said this?',
+      type: 'string'
+    }
+  ]
+}
