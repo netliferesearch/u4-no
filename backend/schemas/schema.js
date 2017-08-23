2658f578dc8ae9983b1862f0f4840656378cb7d0
kmelve <knut@netliferesearch.com>
2017-08-23 15:42:13 +0200

    add more types to schema

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -3,15 +3,40 @@ import publication from './publication';
 import nugget from './nugget';
 import topics from './topics';
 import person from './person';
-import term from './term'
+import term from './term';
+import publicationType from './publicationType';
+import keyword from './keyword';
+import blurb from './blurb';
+
+const pullQuote = {
+    title: 'Pull Quote',
+    name: 'pullQuote',
+    type: 'object',
+    fields: [
+      {
+        name: 'text',
+        type: 'text'
+      }
+    ],
+    preview: {
+      select: {
+        title: 'text'
+      }
+    }
+  }
+
 
 export default createSchema({
   name: 'default',
   types: [
+    pullQuote,
     nugget,
     topics,
     person,
     publication,
+    publicationType,
     term,
+    keyword,
+    blurb
 ]
 })
