9a1a869eb3525ebb2ad782c209e2ce391f5d290c
kmelve <knut@netliferesearch.com>
2017-08-23 14:52:56 +0200

    add a bunch of fields to publication

--- backend/schemas/publication.js
+++ backend/schemas/publication.js
@@ -13,6 +13,47 @@ export default {
     fields: [
       title,
       {
+        name: 'lead',
+        title: 'Lead text',
+        type: 'text',
+        description: 'One paragraph – max 80 words'
+      },
+      {
+        name: 'content',
+        title: 'Publication content',
+        type: 'array',
+        of: [
+          {
+            type: 'block'
+          },
+          {
+            type: 'reference',
+            to: [
+              {
+                type: 'nugget'
+              },
+            ]
+          },
+          {
+            type: 'pullQuote'
+          },
+          {
+            type: 'image'
+          },
+        ]
+      },
+      {
+        name: 'mainPoints',
+        title: 'Main points',
+        description: '2–5 implications/recommendations/must-knows relevant for donors',
+        type: 'array',
+        of: [
+          {
+            type: 'string',
+          }
+        ]
+      },
+      {
         name: 'authors',
         type: 'array',
         of: [
@@ -26,7 +67,95 @@ export default {
           }
         ]
       },
+      {
+        name: 'editors',
+        title: 'Series editors',
+        type: 'array',
+        of: [
+          {
+            type: 'reference',
+            to: [
+              {
+                type: 'person'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'acknowledgements',
+        type: 'text'
+      },
+      {
+        name: 'abstract',
+        type: 'text'
+      },
       date,
+      {
+        name: 'keywords',
+        type: 'array',
+        of: [
+          {
+            type: 'reference',
+            to: [
+              {
+                type: 'keyword'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        name: 'bibliograpicalOverride',
+        title: 'Override Bibliography',
+        type: 'object',
+        options: {
+          collapsable: true,
+        },
+        fields: [
+          {
+            name: 'year',
+            type: 'date'
+          },
+          {
+            name: 'volume',
+            type: 'number'
+          },
+          {
+            name: 'series',
+            type: 'string'
+          },
+          {
+            name: 'issue',
+            type: 'number'
+          },
+          {
+            name: 'publisher',
+            type: 'string'
+          },
+          {
+            name: 'location',
+            type: 'string'
+          },
+          {
+            name: 'pages',
+            type: 'number'
+          },
+          {
+            name: 'type',
+            type: 'string'
+          }
+        ]
+      },
+      {
+        name: 'blurbs',
+        type: 'array',
+        of: [
+          {
+            type: 'blurb'
+          }
+        ]
+      },
       {
         name: 'topics',
         type: 'array',
