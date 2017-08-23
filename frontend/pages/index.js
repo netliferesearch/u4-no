dac537c9e834cd2c0a06bcaa5493420687026cb9
Robert Sjursen <robert.sjursen@cmi.no>
2017-08-23 14:45:36 +0200

    Fetch data from sanity

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,26 @@
-export default () => (
-  <div>Welcome to u4!</div>
-)
+import React, { Component } from 'react';
+const sanityClient = require( '@sanity/client' );
+
+export default class extends Component {
+  static async getInitialProps ({ req }) {
+    const client = sanityClient({
+      projectId: '1f1lcoov',
+      dataset: 'production',
+      token: ''
+    });
+    const publications = await client.fetch('*[_type in ["publication"]]');
+    return {publications}
+  }
+  constructor (props) {
+    super(props);
+    const {publications} = props;
+    this.state = {publications};
+  }
+  render () {
+    return ( <div>Input:
+      <ul>
+        {this.state.publications.map(pub => <li>{pub.title}</li>)}
+      </ul>
+      </div>)
+  }
+}
