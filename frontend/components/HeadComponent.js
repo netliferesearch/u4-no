36a7061589c14d6701cb8eacd394eda334fd54b7
kmelve <knut@netliferesearch.com>
2017-08-25 12:32:49 +0200

    add Head component

---
+++ frontend/components/HeadComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Head from 'next/head';
+
+const HeadComponent = ({ title, description }) =>
+  <Head>
+    <title>{title}</title>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta name="description" content={description} />
+    <link rel="stylesheet" href="/static/css/main.css" />
+  </Head>;
+
+/**
+ * propTypes
+ * @property {object} [object] - The content for the service item
+ */
+HeadComponent.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+
+HeadComponent.defaultProps = {
+  title: '',
+  description: '',
+};
+
+export default HeadComponent;
