9d19e19e5eb41e892dd2748bac7634e673c4fcf2
Nils Norman Haukås <nils@thunki.com>
2017-08-25 11:03:48 +0200

    update frontpage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-const sanityClient = require( '@sanity/client' );
+import Link from 'next/link';
 
 export default class extends Component {
-  static async getInitialProps ({ req }) {
-    const client = sanityClient({
-      projectId: '1f1lcoov',
-      dataset: 'production',
-      token: ''
-    });
-    const publications = await client.fetch('*[_type in ["publication"]]');
-    return {publications}
-  }
   constructor (props) {
     super(props);
-    const {publications} = props;
-    this.state = {publications};
   }
   render () {
-    return ( <div>Input:
-      <ul>
-        {this.state.publications.map(pub => <li>{pub.title}</li>)}
-      </ul>
+    return ( <div>
+      <p>Velkommen til u4.no</p>
+        <ul>
+          <li><Link href={`/publications`}><a>Publikasjoner</a></Link></li>
+        </ul>
       </div>)
   }
 }
