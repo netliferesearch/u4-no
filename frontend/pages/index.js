26019f67f9f3cfd15c1742391f5bc64622828223
kmelve <knut@netliferesearch.com>
2017-08-29 11:08:43 +0200

    make component stateless

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import {
-  Layout
-} from '../components'
+  Layout,
+} from '../components';
 
-export default class extends Component {
-  constructor (props) {
-    super(props);
-  }
-  render () {
-    return (
-      <Layout>
-      <h1>Velkommen til u4.no</h1>
-        <ul>
-          <li><Link href={`/publications`}><a>Publikasjoner</a></Link></li>
-        </ul>
-      </Layout>
-    )
-  }
-}
+export default () => (
+  <Layout>
+    <h1>Velkommen til u4.no</h1>
+    <ul>
+      <li><Link href={'/publications'}><a>Publikasjoner</a></Link></li>
+    </ul>
+  </Layout>
+);
