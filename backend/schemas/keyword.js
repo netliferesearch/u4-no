6b1b6fc651d972f307ed70f083ae6065dcc2351a
Nils Norman Haukås <nils@thunki.com>
2017-08-31 13:16:54 +0200

    Add isCountry flag to keyword
    Some keywords represent countries. This is information from the
    old data and we figured it might be smart to retain this data.

--- backend/schemas/keyword.js
+++ backend/schemas/keyword.js
@@ -5,6 +5,11 @@ export default {
     {
       name: 'keyword',
       type: 'string'
+    },
+    {
+      title: 'Does this keyword represent a country?',
+      name: 'isCountry',
+      type: 'boolean'
     }
   ],
   preview: {
