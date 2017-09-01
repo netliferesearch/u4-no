55447f79cde55e79a2758382706d8521a2a18b1c
kmelve <knut@netliferesearch.com>
2017-09-01 17:45:05 +0200

    restructure article to look more similar to sketches (wip)

--- frontend/components/Article.js
+++ frontend/components/Article.js
@@ -2,27 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import BlockContent from '@sanity/block-content-to-react';
 
-const Article = ({
-  title = "No title",
-  subtitle = "No subtitle",
-  _updatedAt = "No date",
-  lead = "No lead",
-  content = []
-}) => (
-  <article>
+const Article = ({ title = 'No title', subtitle = 'No subtitle', _updatedAt = 'No date', lead = 'No lead', content = [] }) => (
+  <article className="o-wrapper">
     <header className="c-article-header">
-      <p className="c-article-header__issue">U4 issue | Jul 2017</p>
-      <h1 className="c-article-header__title">{title}</h1>
-      <p className="c-article-header__subtitle">{subtitle}</p>
-      <section className="c-article-header__info">
-        <p>By Anthony Nownes - July 2017</p>
-        <p>{_updatedAt}</p>
-        <p>Bergen: Chr. Michelsen Institute (U4 Issue 8-2017) 28 p.</p>
-      </section>
-      <p className="c-article-header__lead">{lead}</p>
+      <div className="o-layout">
+        <div className="o-layout__item u-1/2">
+          <section className="c-article-meta">
+            <section className="c-article-meta__info">
+              <p className="c-article-meta__issue">U4 issue | Jul 2017 | By Anthony Nownes - July 2017 – {_updatedAt} - Bergen: Chr. Michelsen Institute (U4 Issue 8-2017) – 28 minutes.</p>
+            </section>
+          </section>
+        </div>
+        <div className="o-layout__item u-1/1">
+          <h1 className="c-article-header__title">{title}</h1>
+          <p className="c-article-header__subtitle">{subtitle}</p>
+        </div>
+        <div className="o-layout__item u-1/3" />
+        <div className="o-layout__item u-2/3">
+          <p className="c-article-header__lead">{lead}</p>
+        </div>
+      </div>
     </header>
     <main>
-      <BlockContent blocks={content}/>
+      <div className="o-wrapper--huge">
+        <BlockContent blocks={content.filter(block => !['reference', 'pullQuote'].includes(block._type))} />
+      </div>
     </main>
   </article>
 );
