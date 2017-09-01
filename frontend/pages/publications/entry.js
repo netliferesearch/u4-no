11d70c7deee25b8dfec5efaafa822192b51cdd56
kmelve <knut@netliferesearch.com>
2017-09-01 17:43:28 +0200

    linting

--- frontend/pages/publications/entry.js
+++ frontend/pages/publications/entry.js
@@ -1,35 +1,36 @@
-import React, {Component} from 'react';
-import {Layout, Article} from '../../components';
+import React, { Component } from 'react';
+import { Layout, Article } from '../../components';
+
 const sanityClient = require('@sanity/client');
 
 export default class extends Component {
-  static async getInitialProps({query}) {
-    const client = sanityClient({projectId: '1f1lcoov', dataset: 'production', token: ''});
+  static async getInitialProps({ query }) {
+    const client = sanityClient({ projectId: '1f1lcoov', dataset: 'production', token: '' });
     const {
-      id = ''
+      id = '',
     } = query;
-    const sanityQuery = `*[_id == "${id}"]`
+    const sanityQuery = `*[_id == "${id}"]`;
     const publication = (await client.fetch(sanityQuery))[0];
-    return {publication}
+    return { publication };
   }
   constructor(props) {
     super(props);
     const {
       publication = {
-        title: "loading publication"
-      }
+        title: 'loading publication',
+      },
     } = props;
     this.state = {
-      publication
+      publication,
     };
   }
   render() {
-    const {publication} = this.props
+    const { publication } = this.props;
     return (
       <Layout>
-        <Article {...publication}/>
+        <Article {...publication} />
       </Layout>
 
-    )
+    );
   }
 }
