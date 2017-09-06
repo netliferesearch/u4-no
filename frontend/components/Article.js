9f09f604793ff5cc3f120eb09f82386b65632903
maris <mariselnes@users.noreply.github.com>
2017-09-06 14:24:06 +0200

    refactor article

--- frontend/components/Article.js
+++ frontend/components/Article.js
@@ -11,23 +11,13 @@ const Article = ({
 }) => (
   <article className="o-wrapper">
     <header className="c-article-header o-wrapper--huge">
-      <div className="o-layout">
-        <div className="o-layout__item u-1/1">
-          <div className="c-article-header__issue"><a href="#">U4 issue </a>| NRE</div>
-        </div>
-        <div className="o-layout__item u-1/1">
-          <h1 className="c-article-header__title">{title}</h1>
-          <p className="c-article-header__subtitle">{subtitle}</p>
-        </div>
-        <div className="o-layout__item u-1/1">
-          <div className="c-article-header__byline">By <a href="#">Åse Gilje Østensen</a> & <a href="#">Mats Stridsman </a>
-             | Bergen: Chr. Michelsen Institute (U4 Issue 2017:3)</div>
-           <div className="c-article-header__photo-credit">Photography by <a href="#">Dani Deahl</a></div>
-        </div>
-        <div className="o-layout__item u-1/1">
-          <p className="c-article-header__lead">{lead}</p>
-        </div>
-      </div>
+      <div className="c-article-header__issue"><a href="#">U4 issue </a>| NRE</div>
+      <h1 className="c-article-header__title">{title}</h1>
+      <p className="c-article-header__subtitle">{subtitle}</p>
+      <div className="c-article-header__byline">By <a href="#">Åse Gilje Østensen</a> & <a href="#">Mats Stridsman </a>
+         | Bergen: Chr. Michelsen Institute (U4 Issue 2017:3)</div>
+      <div className="c-article-header__photo-credit">Photography by <a href="#">Dani Deahl</a></div>
+      <p className="c-article-header__lead">{lead}</p>
     </header>
     <main>
       <div className="c-article o-wrapper--huge">
