d4092b194345d8d0ec845ee04a78c1ff81812792
Robert Sjursen <robert.sjursen@cmi.no>
2017-09-06 12:55:06 +0200

    Add download-old-data.sh

---
+++ old-data-import/download-old-data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'Downloading authors'
+curl http://www.u4.no/partner/admin/sanity/authors.cfm > authors.ndjson
+echo 'Downloading tags'
+curl http://www.u4.no/partner/admin/sanity/tags.cfm > tags.ndjson
+echo 'Downloading publications'
+curl http://www.u4.no/partner/admin/sanity/publications.cfm > publications.ndjson
+echo 'Finished downloading'
