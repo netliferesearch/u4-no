ce58d884fc552d53799444a072c4f1cc19615e7d
maris <mariselnes@users.noreply.github.com>
2017-09-07 15:37:52 +0200

    changed style on article nugget

--- frontend/components/Article.js
+++ frontend/components/Article.js
@@ -31,7 +31,7 @@ const blockHandlers = {
 const typeHandlers = {
   image: ({ attributes }) => <Figure {...attributes} />,
   pullQuote: ({ attributes: { text } }) => <div className="c-article__pullQuote">{text}</div>,
-  nugget: ({ attributes: { text, title } }) => <div className="c-article__nugget"><h2>{title}</h2><BlockContent blocks={text} /></div>,
+  nugget: ({ attributes: { text, title } }) => <div className="c-article__nugget"><h2 className="c-article__nugget-title">{title}</h2><BlockContent  blocks={text} /></div>,
 };
 
 const Article = ({ title = 'No title', subtitle = 'No subtitle', _updatedAt = 'No date', lead = 'No lead', content = [] }) => (
