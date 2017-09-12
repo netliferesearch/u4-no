20b129fe91ace86c5c604cae70ce06ef6eb9853a
kmelve <knut@netliferesearch.com>
2017-09-12 09:35:53 +0200

    add smoothscroll support

--- frontend/components/Article.js
+++ frontend/components/Article.js
@@ -5,7 +5,7 @@ import slugify from 'slugify';
 import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor';
 import ArticleContents from './ArticleContents';
 import randomKey from '../helpers/randomKey';
-configureAnchors({ offset: -60, scrollDuration: 200, keepLastAnchorHash: true })
+configureAnchors({ offset: -60, scrollDuration: 400, keepLastAnchorHash: true });
 
 const Figure = ({asset, caption, license, licensor}) => (<figure className="c-article__figure">
   <img className="c-article__figure-img" src={asset.url} alt={asset.altText} />
@@ -23,7 +23,7 @@ const blockHandlers = {
     listItem: ({ children = [] }) => <li key={randomKey()}>{children}</li>,
   },
   textBlock: {
-    normal: ({ children = [] }) => <p className="lede">{children}</p>,
+    normal: ({ children = [] }) => <p>{children}</p>,
     h2: ({ children = [] }) => <ScrollableAnchor id={slugify(children[0], { lower: true })}><h2>{children}</h2></ScrollableAnchor>,
   },
 };
@@ -31,7 +31,7 @@ const blockHandlers = {
 const typeHandlers = {
   image: ({ attributes }) => <Figure {...attributes} />,
   pullQuote: ({ attributes: { text } }) => <div className="c-article__pullQuote">{text}</div>,
-  nugget: ({ attributes: { text, title } }) => <div className="c-article__nugget"><h2 className="c-article__nugget-title">{title}</h2><BlockContent  blocks={text} /></div>,
+  nugget: ({ attributes: { text, title } }) => <div className="c-article__nugget"><h2 className="c-article__nugget-title">{title}</h2><BlockContent blocks={text} /></div>,
 };
 
 const Article = ({ title = 'No title', subtitle = 'No subtitle', _updatedAt = 'No date', lead = 'No lead', content = [] }) => (
@@ -40,16 +40,30 @@ const Article = ({ title = 'No title', subtitle = 'No subtitle', _updatedAt = 'N
       <ArticleContents content={content} />
     </div>
     <header className="c-article-header o-wrapper--huge">
-      <div className="c-article-header__issue"><a href="#">U4 issue </a>| NRE</div>
+
       <h1 className="c-article-header__title">{title}</h1>
       <p className="c-article-header__subtitle">{subtitle}</p>
       <div className="c-article-header__byline">By <a href="#">Åse Gilje Østensen</a> & <a href="#">Mats Stridsman </a>
          | Bergen: Chr. Michelsen Institute (U4 Issue 2017:3)</div>
       <div className="c-article-header__photo-credit">Photography by <a href="#">Dani Deahl</a></div>
-      <div className="c-article-header__topics">
-        <div className="c-article-header__topics-item">Anti-Corruption</div>
-        <div className="c-article-header__topics-item">Poverty sanitation</div>
-        <div className="c-article-header__topics-item">Water sector</div>
+      <div className="c-article-header__summary-for-busy-people">
+        <details>
+          <summary>Main points for busy people</summary>
+          <ol>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Et facere nostrum itaque at blanditiis nesciunt rem optio eaque qui eligendi?
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Et facere nostrum itaque at blanditiis nesciunt rem optio eaque qui eligendi?
+            </li>
+            <li>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Et facere nostrum itaque at blanditiis nesciunt rem optio eaque qui eligendi?
+            </li>
+          </ol>
+        </details>
       </div>
       <p className="c-article-header__lead">{lead}</p>
     </header>
@@ -62,6 +76,35 @@ const Article = ({ title = 'No title', subtitle = 'No subtitle', _updatedAt = 'N
         />
       </div>
     </main>
+    <footer>
+      <details>
+        <summary>
+          Sharing
+        </summary>
+        <li>Twitter highlight thingy</li>
+        <li>Smart email thingy</li>
+        <li>Link to this section stuff</li>
+      </details>
+      <details>
+        <summary>Abstract</summary>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Qui, earum velit reiciendis ullam sapiente quidem pariatur repudiandae dolorem
+          amet vel rem vitae delectus minima, vero numquam totam obcaecati eligendi blanditiis?</p>
+      </details>
+      <details>
+        <summary>Acknowledgements</summary>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Qui, earum velit reiciendis ullam sapiente quidem pariatur repudiandae dolorem
+          amet vel rem vitae delectus minima, vero numquam totam obcaecati eligendi blanditiis?</p>
+      </details>
+      <details>
+        <summary>We also recommend</summary>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Qui, earum velit reiciendis ullam sapiente quidem pariatur repudiandae dolorem
+          amet vel rem vitae delectus minima, vero numquam totam obcaecati eligendi blanditiis?</p>
+      </details>
+
+    </footer>
   </article>
 );
 
