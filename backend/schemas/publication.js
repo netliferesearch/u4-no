6b0d11a69528121b23aa8d049726bc103b1acf60
kmelve <knut@netliferesearch.com>
2017-09-12 09:35:33 +0200

    add featured image

--- backend/schemas/publication.js
+++ backend/schemas/publication.js
@@ -1,4 +1,5 @@
 import FunkyEditor from '../components/FunkyEditor'
+import license from './fields/license';
 /**
  * A publication is a long form document
  */
@@ -19,6 +20,36 @@ export default {
       title,
       subtitle,
       leadText,
+      {
+        name: 'featuredImage',
+        title: 'Featured image',
+        description: 'This is the image that illustrates this publication in the hero, frontpage and previews',
+        type: 'image',
+        options: {
+          isHighlighted: true,
+          hotspot: true,
+        },
+        fields: [
+          {
+            name: 'altText',
+            title: 'Alternative text',
+            description: 'For users that can\'t see images',
+            type: 'string'
+          },
+          {
+            name: 'caption',
+            title: 'Caption text',
+            description: '',
+            type: 'string'
+          },
+          {
+            name: 'licensor',
+            title: 'Lisence',
+            type: 'text'
+          },
+          license,
+        ]
+      },
       {
         name: 'content',
         title: 'Publication content',
