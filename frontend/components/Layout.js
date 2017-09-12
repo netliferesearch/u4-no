07fc3a30b0f8d0052ac1a735dc55a2bf6721f07b
kmelve <knut@netliferesearch.com>
2017-09-12 09:35:56 +0200

    add header

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -7,6 +7,13 @@ import {
 export default ({title, description, children = []}) => (
   <div>
     <HeadComponent title={title} description={description} />
+    <div className="o-wrapper">
+      <div className="c-top-bar">
+        <a href="/">
+          <img className="c-top-bar__logo" alt="The U4 logo" src="/static/u4-logo.svg" />
+        </a>
+      </div>
+    </div>
     {children}
   </div>
 )
