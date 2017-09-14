e2388caa5ea2c9b1732f81792bf14b6c61a5a3ba
Robert Sjursen <robert.sjursen@cmi.no>
2017-09-14 15:27:16 +0200

    Add deleteObsoleteKeywords.js

---
+++ old-data-import/deleteObsoleteKeywords.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const sanityClient = require('@sanity/client');
+
+const client = sanityClient({
+  projectId: '1f1lcoov',
+  dataset: 'production',
+  token: process.env.SANITY_TOKEN,
+});
+
+async function main() {
+  const docs = await client.fetch("*[_type == 'keyword' && keyword match 'Delete']");
+  for (const doc of docs) {
+    await client.delete(doc._id);
+    console.log('Deleted ', doc._id);
+  }
+  console.log('done deleting keywords marked for deletion');
+}
+
+main();
