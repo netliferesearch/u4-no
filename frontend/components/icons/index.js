133d95a085234f1d60c48dc1866483f2b00c601d
kmelve <knut@netliferesearch.com>
2017-09-20 07:49:07 +0200

    add icons

---
+++ frontend/components/icons/index.js
@@ -0,0 +1,4 @@
+export { default as Basics } from './Basics';
+export { default as Picture } from './Picture';
+export { default as Publication } from './Publication';
+export { default as Resources } from './Resources';
