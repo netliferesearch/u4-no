4ba00b8813f2d97ec6e3382ce3374c9dcdd61372
Nils Norman Haukås <nils@thunki.com>
2017-09-20 13:30:48 +0200

    Refactor. Test page components with enzyme.

---
+++ frontend/pages/publication.entry.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import PublicationEntry from './publication.entry';
+
+test('publication entry page', async () => {
+  const props = await PublicationEntry.getInitialProps({
+    query: { id: 'f62b433d-9bbf-4bcb-8a4d-9aed37e5afcd' },
+  });
+  expect(shallow(<PublicationEntry {...props} />).exists()).toBe(true);
+});
