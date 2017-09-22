b2f6dd8e9a9b4f7f884caefd75456993b7de96b6
kmelve <knut@netliferesearch.com>
2017-09-22 10:27:28 +0200

    add options and license fields to featured image

--- backend/schemas/fields/featuredImage.js
+++ backend/schemas/fields/featuredImage.js
@@ -1,4 +1,30 @@
+import license from './license';
+
 export default {
   name: 'featuredImage',
   type: 'image',
+  options: {
+    isHighlighted: true,
+    hotspot: true,
+  },
+  fields: [
+    {
+      name: 'altText',
+      title: 'Alternative text',
+      description: 'For users that can\'t see images',
+      type: 'string'
+    },
+    {
+      name: 'caption',
+      title: 'Caption text',
+      description: '',
+      type: 'string'
+    },
+    {
+      name: 'licensor',
+      title: 'Lisence',
+      type: 'text'
+    },
+    license,
+  ]
 }
