671a78d9e925a4576df05e070896a8d6ca23c5b6
kmelve <knut@netliferesearch.com>
2017-09-27 09:44:26 +0200

    add standfirst field

---
+++ backend/schemas/fields/standfirst.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'standfirst',
+  type: 'string',
+  description: `Quick promo-text for other channels. What is the article about? Say one thing that convince readers that they will lose out by not reading it.
+
+(Aim for: 170–200 characters with spaces)
+`
+}
