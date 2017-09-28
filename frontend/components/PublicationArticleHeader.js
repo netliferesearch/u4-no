863079726fa299428864a92c63cf30f62adcecbf
Robert Sjursen <robert.sjursen@cmi.no>
2017-09-28 14:59:53 +0200

    Add publication type to PublicationArticleHeader

--- frontend/components/PublicationArticleHeader.js
+++ frontend/components/PublicationArticleHeader.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from '../routes';
-import randomKey from '../helpers/randomKey'
+import randomKey from '../helpers/randomKey';
 
 const PublicationArticleHeader = ({
   title = 'no title',
@@ -8,11 +8,12 @@ const PublicationArticleHeader = ({
   lead = 'no lead',
   topics = [],
   className = '',
+  publicationType = {},
 }) => (
   <header className={`${className} c-article-header`}>
     {/* Wrap in standard grid width until we know better */}
     <div className="c-article-header__meta c-article-header__meta-uppercase">
-      U4 Brief  |{' '}
+      {publicationType.title && `${publicationType.title} | `}
       {topics.map(({ title = '', _id = '' }) => (
         <a href key={_id}>
           {title}
@@ -25,17 +26,23 @@ const PublicationArticleHeader = ({
     <div>
       <p className="c-article-header__subtitle">{subtitle}</p>
       <div className="c-article-header__meta">
-        <p>By <a href="#">Åse Gilje Østensen</a> & <a href="#">Mats Stridsman</a><br></br>
-        Series Editor <a href="#">Kendra Dupuy</a><br></br>
-        Bergen: U4 Anti-Corruption Resource Centre @ Chr. Michelsen Institute (U4 Brief 2017:5)</p>
-      <p><a href="#">Also available in Spanish</a></p>
+        <p>
+          By <a href="#">Åse Gilje Østensen</a> & <a href="#">Mats Stridsman</a>
+          <br />
+          Series Editor <a href="#">Kendra Dupuy</a>
+          <br />
+          Bergen: U4 Anti-Corruption Resource Centre @ Chr. Michelsen Institute (U4 Brief 2017:5)
+        </p>
+        <p>
+          <a href="#">Also available in Spanish</a>
+        </p>
       </div>
-        <Link route="/">
-          <a className="c-article-header__button">
-            <div className="c-article-header__button-text">Summary for busy people</div>
-            <div className="c-article-header__button-icon">→</div>
-          </a>
-        </Link>
+      <Link route="/">
+        <a className="c-article-header__button">
+          <div className="c-article-header__button-text">Summary for busy people</div>
+          <div className="c-article-header__button-icon">→</div>
+        </a>
+      </Link>
     </div>
   </header>
 );
