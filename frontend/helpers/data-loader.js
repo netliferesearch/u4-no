87eb68912bdb1bc006a9955f58f4e3ceaa5f7d52
Nils Norman Haukås <nils@thunki.com>
2017-09-29 11:01:41 +0200

    add redux to data-loader

--- frontend/helpers/data-loader.js
+++ frontend/helpers/data-loader.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import withRedux from 'next-redux-wrapper';
 import sanityClient from '@sanity/client';
+import { initStore, startClock, addCount } from './redux-store';
+
 import materialize from '../helpers/materialize';
 
+const mapDispatchToProps = dispatch => ({
+  addCount: bindActionCreators(addCount, dispatch),
+  startClock: bindActionCreators(startClock, dispatch),
+});
+
 export default (Child, { queryFunc = false, materializeDepth = false }) =>
-  class extends Component {
-    static async getInitialProps(nextContext) {
-      const client = sanityClient({
-        projectId: '1f1lcoov',
-        dataset: 'production',
-        token: '',
-        useCdn: false,
-      });
-      if (!queryFunc) {
-        console.log('No query function provided. Returning empty object');
-        return {};
+  withRedux(initStore, null, mapDispatchToProps)(
+    class DataLoader extends Component {
+      static async getInitialProps(nextContext) {
+        const client = sanityClient({
+          projectId: '1f1lcoov',
+          dataset: 'production',
+          token: '',
+          useCdn: false,
+        });
+        if (!queryFunc) {
+          console.log('No query function provided. Returning empty object');
+          return {};
+        }
+        const { sanityQuery, param } = queryFunc(nextContext);
+        const sanityResults = await client.fetch(sanityQuery, param);
+        if (!materializeDepth) {
+          return sanityResults;
+        }
+        return materialize(sanityResults, materializeDepth);
       }
-      const { sanityQuery, param } = queryFunc(nextContext);
-      const sanityResults = await client.fetch(sanityQuery, param);
-      if (!materializeDepth) {
-        return sanityResults;
+      render() {
+        return (
+          <div>
+            <Child {...this.props} />
+          </div>
+        );
       }
-      return materialize(sanityResults, materializeDepth);
-    }
-    render() {
-      return (
-        <div>
-          <Child {...this.props} />
-        </div>
-      );
-    }
-  };
+    },
+  );
---
+++ frontend/helpers/redux-store.js
@@ -0,0 +1,40 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunkMiddleware from 'redux-thunk';
+
+const exampleInitialState = {
+  lastUpdate: 0,
+  light: false,
+  count: 0,
+};
+
+export const actionTypes = {
+  ADD: 'ADD',
+  TICK: 'TICK',
+};
+
+// REDUCERS
+export const reducer = (state = exampleInitialState, action) => {
+  switch (action.type) {
+    case actionTypes.TICK:
+      return Object.assign({}, state, { lastUpdate: action.ts, light: !!action.light });
+    case actionTypes.ADD:
+      return Object.assign({}, state, {
+        count: state.count + 1,
+      });
+    default:
+      return state;
+  }
+};
+
+// ACTIONS
+export const serverRenderClock = isServer => dispatch =>
+  dispatch({ type: actionTypes.TICK, light: !isServer, ts: Date.now() });
+
+export const startClock = () => dispatch =>
+  setInterval(() => dispatch({ type: actionTypes.TICK, light: true, ts: Date.now() }), 800);
+
+export const addCount = () => dispatch => dispatch({ type: actionTypes.ADD });
+
+export const initStore = (initialState = exampleInitialState) =>
+  createStore(reducer, initialState, composeWithDevTools(applyMiddleware(thunkMiddleware)));
