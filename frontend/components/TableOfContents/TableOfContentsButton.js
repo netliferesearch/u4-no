64081fa3cac3d5e3feacdc34f2e57cbda8dccf15
Nils Norman Haukås <nils@thunki.com>
2017-10-05 11:01:32 +0200

    use redux to store whether or not menu is open

--- frontend/components/TableOfContents/TableOfContentsButton.js
+++ frontend/components/TableOfContents/TableOfContentsButton.js
@@ -1,34 +1,33 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 import TableOfContentsBase from './TableOfContentsBase';
+import { toggleArticleMenu } from '../../helpers/redux-store';
 
-export default class extends Component {
+export class TableOfContentsButton extends Component {
   constructor(props) {
     super(props);
     this.state = { menuOpen: false };
-    this.buttonClickHandler = this.buttonClickHandler.bind(this);
-    this.tocItemHandler = this.tocItemHandler.bind(this);
-  }
-
-  buttonClickHandler() {
-    this.setState(prevState => ({ menuOpen: !prevState.menuOpen }));
-  }
-
-  tocItemHandler() {
-    this.setState(prevState => ({ menuOpen: false }));
   }
 
   render() {
-    const { content = [] } = this.props;
+    const { content = [], isArticleMenuOpen = false, toggleArticleMenu = () => {} } = this.props;
     return (
-      <div className={this.state.menuOpen ? 'c-toc-mobile c-toc-mobile--open' : 'c-toc-mobile'}>
+      <div className={isArticleMenuOpen ? 'c-toc-mobile c-toc-mobile--open' : 'c-toc-mobile'}>
         <div className="c-toc-mobile__menu">
           <h2>Table of contents</h2>
-          <TableOfContentsBase onItemSelected={this.tocItemHandler} content={content} />
+          <TableOfContentsBase onItemSelected={toggleArticleMenu} content={content} />
         </div>
-        <button className="c-toc-mobile__button" onClick={this.buttonClickHandler}>
+        <button className="c-toc-mobile__button" onClick={toggleArticleMenu}>
           <img alt="Table of contents icon" src="/static/table-of-contents-icon.svg" />
         </button>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ isArticleMenuOpen }) => ({ isArticleMenuOpen });
+const mapDispatchToProps = dispatch => ({
+  toggleArticleMenu: bindActionCreators(toggleArticleMenu, dispatch),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(TableOfContentsButton);
