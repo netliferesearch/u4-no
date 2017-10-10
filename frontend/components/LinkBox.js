7b26e03e7e706c4f3dc01b4078d6fbd91e7a02d7
thomaslgj <thomas@netliferesearch.com>
2017-10-10 11:23:35 +0200

    Improve linkbox styling

--- frontend/components/LinkBox.js
+++ frontend/components/LinkBox.js
@@ -9,7 +9,7 @@ const LinkBox = ({ title = 'Click me', text = '', icon = '', route = '', params 
         <div className="c-linkbox__content">
           {icon && <div className="c-linkbox__icon">{icon()}</div>}
           <div className="c-linkbox__body">
-            <h2>{title}</h2>
+            <h2 className="c-linkbox__title">{title}</h2>
             {text && <p>{text}</p>}
           </div>
           <div className="c-linkbox__arrow">
