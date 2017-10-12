28172540aa6e21fb6272cb7f5f5e47015c26220a
Nils Norman Haukås <nils@thunki.com>
2017-10-12 15:32:58 +0200

    pick right longform gridclass depending on the amount of content inside it

--- frontend/components/LongformArticle.js
+++ frontend/components/LongformArticle.js
@@ -29,63 +29,37 @@ const serializers = {
       // Heading?
       if (/^h\d/.test(style)) {
         const level = parseInt(style.slice(1), 10);
-        const id = level === 2 || level === 3
-          ? slugify(children[0], { lower: true })
-          : undefined;
+        const id = level === 2 || level === 3 ? slugify(children[0], { lower: true }) : undefined;
 
-        return React.createElement(
-          style,
-          { id, className: 'c-longform-grid__standard' },
-          children,
-        );
+        return React.createElement(style, { id, className: 'c-longform-grid__standard' }, children);
       }
 
       if (style === 'blockquote') {
-        return (
-          <blockquote className="c-longform-grid__large-right">
-            {children}
-          </blockquote>
-        );
+        return <blockquote className="c-longform-grid__large-right">{children}</blockquote>;
       }
 
-      return (
-        <p className="c-longform-grid__standard ">
-          {children}
-        </p>
-      );
+      return <p className="c-longform-grid__standard ">{children}</p>;
     },
   },
 
   list: ({ type, children }) => {
     if (type === 'bullet') {
-      return (
-        <ul className="list-bullets c-longform-grid__standard ">
-          {children}
-        </ul>
-      );
+      return <ul className="list-bullets c-longform-grid__standard ">{children}</ul>;
     }
 
-    return (
-      <ol className="list-numbered c-longform-grid__standard ">
-        {children}
-      </ol>
-    );
+    return <ol className="list-numbered c-longform-grid__standard ">{children}</ol>;
   },
 };
 
-class LongformArticle extends Component {
-  render() {
-    const { content = [] } = this.props;
-    const blocks = content.filter(block => !['reference'].includes(block._type));
-    return (
-      <main className="c-article c-longform-grid-sub-div">
-        <BlockContent
-          blocks={blocks}
-          serializers={serializers}
-        />
-      </main>
-    );
-  }
-}
+const LongformArticle = ({ content = [] }) => {
+  const blocks = content.filter(block => !['reference'].includes(block._type));
+  return (
+    <main
+      className={`c-article ${blocks.length === 1 ? 'c-longform-grid' : 'c-longform-grid-sub-div'}`}
+    >
+      <BlockContent blocks={blocks} serializers={serializers} />
+    </main>
+  );
+};
 
 export default LongformArticle;
