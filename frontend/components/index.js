5f996b16cc1fbd46b24172ab0252c8e2347a79bd
kmelve <knut@netliferesearch.com>
2017-10-24 17:08:45 +0200

    add SearchResults to index, sort components

--- frontend/components/index.js
+++ frontend/components/index.js
@@ -11,16 +11,17 @@ export { default as LinkList } from './LinkList';
 export { default as LongformArticle } from './LongformArticle';
 export { default as Mosaic } from './Mosaic';
 export { default as Newsletter } from './Newsletter';
+export { default as PartnerPromo } from './PartnerPromo';
 export { default as Person } from './Person';
 export { default as PublicationArticleHeader } from './PublicationArticleHeader';
 export { default as PullQuote } from './PullQuote';
+export { default as SearchResults } from './SearchResults';
 export { default as Team } from './Team';
-export { default as PartnerPromo } from './PartnerPromo';
 export { default as ToggleBlock } from './ToggleBlock';
 export {
+  CustomScrollSpy,
   TableOfContentsButton,
   TableOfContentsContent,
   TableOfContentsSidebar,
-  CustomScrollSpy,
 } from './TableOfContents';
 export { default as Logo } from './Logo';
