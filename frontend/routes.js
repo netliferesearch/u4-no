33d3d46b27a1a093a093a92f791ca4e65693598b
kmelve <knut@netliferesearch.com>
2017-10-24 20:50:27 +0200

    add variable slugs in order to correnspond to type

--- frontend/routes.js
+++ frontend/routes.js
@@ -2,14 +2,14 @@ const routes = (module.exports = require('next-routes')());
 
 routes
   .add('main.index', '/')
-  .add('publication.entry', '/publications/:slug')
-  .add('publication.index', '/publications')
+  .add('publication.entry', '/publication(|s)/:slug')
+  .add('publication.index', '/publication(|s)')
   .add('topic.article', '/topics/:slug/:topicPart')
   .add('topic.entry', '/topics/:slug')
   .add('topic.index', '/topics')
   .add('general.article', '/article/:slug')
   .add('search.index', '/search')
-  .add('online-training.entry', '/online-training/:id');
+  .add('online-training.entry', '/online-training/:id')
   .add('online-training.index', '/online-training')
   .add('workshops.index', '/workshops')
   .add('service.entry', '/services/:slug');
