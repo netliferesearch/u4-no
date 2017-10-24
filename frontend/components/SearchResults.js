b842ca082c509ea9308b862f9f1e94915587307e
kmelve <knut@netliferesearch.com>
2017-10-24 13:06:16 +0200

    wip

---
+++ frontend/components/SearchResults.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { results: [] }
+  }
+
+  render() {
+
+    return <ul>
+      <li>Search result</li>
+    </ul>
+  }
+}
