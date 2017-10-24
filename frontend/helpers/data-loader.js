5992ed889ab0056bd3275fa8c3ba89670798af23
kmelve <knut@netliferesearch.com>
2017-10-24 17:11:02 +0200

    log more in data-loader, set default params

--- frontend/helpers/data-loader.js
+++ frontend/helpers/data-loader.js
@@ -24,8 +24,10 @@ export default (Child, { queryFunc = false, materializeDepth = false }) =>
           console.log('No query function provided. Returning empty object');
           return {};
         }
-        const { sanityQuery, param } = queryFunc(nextContext);
+        const { sanityQuery, param = {} } = queryFunc(nextContext);
+        console.log('test', sanityQuery)
         const sanityResults = await client.fetch(sanityQuery, param);
+        console.log('test2', sanityResults)
         if (!materializeDepth) {
           return sanityResults;
         }
