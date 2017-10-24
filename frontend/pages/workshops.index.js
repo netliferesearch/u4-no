937519bd5970118406fd4d2503fd57418a2693ab
thomaslgj <thomas@netliferesearch.com>
2017-10-24 08:37:21 +0200

    Add more stuff to Workshops page

--- frontend/pages/workshops.index.js
+++ frontend/pages/workshops.index.js
@@ -4,11 +4,17 @@ import sanityClient from '@sanity/client';
 import DataLoader from '../helpers/data-loader';
 import Head from 'next/head';
 
-import { Footer, Layout, ExtendedBlockContent, Accordion, LinkList } from '../components';
+import { Footer, Layout, ExtendedBlockContent, Accordion, LinkList, Newsletter } from '../components';
 import { Feature, Mosaic } from '../components';
 import { DownArrowButton, RightArrowButton } from '../components/buttons';
 import { Basics, Picture, Publication, Resources, ResearchAgenda, ArrowRight } from '../components/icons';
 
+const linkListContent = [
+  {
+    title: 'Talk to Sofie',
+    link: '#',
+  },
+];
 
 const Workshops = ({
   topic: {
@@ -27,34 +33,65 @@ const Workshops = ({
 }) => (
   <Layout>
 
-    <h1 className="c-topic-page_title">Online training</h1>
-    <h2 className="c-topic-page__longTitle">Anti-corruption training for development practitioners</h2>
+    <h2 className="c-topic-page__longTitle">Workshops, headquarter visits and events</h2>
 
     <section className="c-boxOnImage u-margin-bottom-huge">
       <figure className="c-boxOnImage__figure">
         <img alt="" src="/static/onlinetraining.png" />
       </figure>
       <div className="c-boxOnImage__body">
-        <p className="c-boxOnImage__lead">Be confident with anti-corruption initiatives in your work.
+        <p className="c-boxOnImage__lead">We facilitate local dialouge
         </p>
-        <p>At U4 we offer dynamic and time-efficient online courses to our U4 Partners or related embassy/foreign ministry. As a partner you can also nominate other development practitioners for a place, or read the course readers for any course, any time. Our Self-paced online course are free to take for everyone.</p>
-        <LinkList content={linkListContent} otherClasses="u-margin-top-none" />
+        <p>Corruption is a sensitive topic. A neutral forum to discuss it openly helps different agents of change find each other. Our advisers facilitate dialogues between development partners at U4 in-country workshops. During the past decade we’ve held over 60 workshops for our partners and their local counterparts. </p>
+
+        <p>We can also assist with staff training and policy advice at headquarters. Or would you like to meet us at a conference? We gladly share our insights at research and policy events, to help advance general anti-corruption knowledge.</p>
+        <LinkList
+          content={[{ title: 'Talk to Sofie', link: '#' }]}
+          otherClasses="u-margin-top-none"
+        />
       </div>
     </section>
 
-    <section className="c-topic-section--small">
-      <div className="c-features">
-        <Feature title={features[0].title} icon={ResearchAgenda} />
-        <Feature title={features[1].title} icon={ResearchAgenda} />
-        <Feature title={features[2].title} icon={ResearchAgenda} />
-        <Feature title={features[3].title} icon={ResearchAgenda} />
-      </div>
-    </section>
 
     <section className="c-topic-section">
-      <h2 className="c-topic-section__title">Upcoming courses</h2>
-      <Mosaic resources={courses} />
+      <h2 className="c-topic-section__title">Expect new knowledge and concrete advice.</h2>
+
+
     </section>
+
+
+    <section className="c-topic-section">
+      <h2 className="c-topic-section__title">We understand how our work, and can contribute to internal processes and policy discussions.</h2>
+
+      <section className="c-boxOnImage u-margin-bottom-huge">
+        <figure className="c-boxOnImage__figure">
+          <img alt="" src="/static/onlinetraining.png" />
+        </figure>
+        <div className="c-boxOnImage__body">
+          <p className="c-boxOnImage__lead">Headquarter visits
+          </p>
+          <p>Our team understands how the U4 partners work, and can contribute to internal processes and policy discussions.</p>
+          <LinkList
+            content={[{ title: 'Read more', link: '#' }]}
+            otherClasses="u-margin-top-none"
+          />
+        </div>
+      </section>
+
+    </section>
+
+
+    <section className="c-topic-section">
+      <h2 className="c-topic-section__title">Upcoming workshops</h2>
+
+
+      <h2 className="c-topic-section__cta">
+        <a href="#">Talk to us &nbsp;<ArrowRight /></a>
+      </h2>
+    </section>
+
+
+    <Newsletter />
 
     <Footer />
   </Layout>
