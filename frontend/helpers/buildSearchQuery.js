1166050e7c8b55062820a23f99bfe51222825066
kmelve <knut@netliferesearch.com>
2017-10-26 14:52:49 +0200

    tune buildquery

--- frontend/helpers/buildSearchQuery.js
+++ frontend/helpers/buildSearchQuery.js
@@ -1,4 +1,44 @@
-export default (value = '') => {
-  const matchString = value.length ? value.split(' ').map(tkn => `"${tkn}*"`).join(',') : value;
-  return `{ "results": *[(title match [${matchString}] || longTitle match [${matchString}] || explainerText match [${matchString}] || firstName match [${matchString}] || surname match [${matchString}] || email match [${matchString}] || subtitle match [${matchString}] || standfirst match [${matchString}] || lead match [${matchString}] || summaryExternal match [${matchString}] || acknowledgements match [${matchString}] || abstract match [${matchString}] || description match [${matchString}] || term match [${matchString}] || definition match [${matchString}] || keyword match [${matchString}] || text match [${matchString}] || origin match [${matchString}] || url match [${matchString}] || resolvedUrl match [${matchString}] || crawledAt match [${matchString}] || name match [${matchString}] || language match [${matchString}] || utc match [${matchString}] || local match [${matchString}] || timezone match [${matchString}])][0...20]{title, slug, date, _type} }`;
+export default ({ queryString = false, limit: { from = 0, to = 20 } = { from: 0, to: 20 } }) => {
+  const matchString = queryString.length ? queryString.split(' ').map(tkn => `"${tkn}*"`).join(',') : queryString;
+  return `{
+    "results":
+    *[
+      (title match [${matchString}]
+        || longTitle match [${matchString}]
+        || explainerText match [${matchString}]
+        || firstName match [${matchString}]
+        || surname match [${matchString}]
+        || email match [${matchString}]
+        || subtitle match [${matchString}]
+        || standfirst match [${matchString}]
+        || lead match [${matchString}]
+        || summaryExternal match [${matchString}]
+        || acknowledgements match [${matchString}]
+        || abstract match [${matchString}]
+        || description match [${matchString}]
+        || term match [${matchString}]
+        || definition match [${matchString}]
+        || keyword match [${matchString}]
+        || text match [${matchString}]
+        || origin match [${matchString}]
+        || url match [${matchString}]
+        || resolvedUrl match [${matchString}]
+        || crawledAt match [${matchString}]
+        || name match [${matchString}]
+        || language match [${matchString}]
+        || utc match [${matchString}]
+        || local match [${matchString}]
+        || timezone match [${matchString}]
+      ) && defined(slug.current)
+    ]
+    [${from}...${to}]
+    {_id,
+      title,
+      slug,
+      date,
+      _type,
+      subtitle,
+      authors->{...},
+
+    }}`.replace('\n', '');
 };
