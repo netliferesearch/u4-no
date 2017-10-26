670100d7246f0521ebc39342d74fce1e24d5b4e8
maris <mariselnes@users.noreply.github.com>
2017-10-26 15:39:31 +0200

    adjust styling on Partner-agencies

--- frontend/components/PartnerAgencies.js
+++ frontend/components/PartnerAgencies.js
@@ -9,11 +9,47 @@ const classes = BEMHelper({
 });
 
 const PartnerAgencies= ({ title }) => (
+
   <section {...classes()}>
-      <div {...classes('wrapper')}>
-        <div {...classes('item')}>
-          <CmiLogo {...classes('color-icon')} /><br /><br />
+    <div className="o-wrapper u-margin-bottom-huge">
+      <div {...classes('heading')}>
+        <h1>Partner Agencies</h1>
+        <p {...classes('text')}>The U4 Anti-Corruption Resource Centre is an institutional partnership
+          of bilateral international development agencies/foreign departments.</p>
         </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
+          <div {...classes('item')}>
+            <CmiLogo {...classes('color-icon')} /><br /><br />
+          </div>
       </div>
   </section>
 );
--- frontend/pages/main.index.js
+++ frontend/pages/main.index.js
@@ -25,6 +25,8 @@ export default () => (
       </section>
     </div>
     <Footer />
-    <PartnerAgencies />
+
+      <PartnerAgencies />
+
   </LayoutHomepage>
 );
