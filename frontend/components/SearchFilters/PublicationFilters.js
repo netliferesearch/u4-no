b94b57b01f91b1b58db1e2f23a5b3c8a68d84b47
Nils Norman Haukås <nils@thunki.com>
2017-10-27 12:19:30 +0200

    publicationFilter: Show all publication types, disable if not in result list and show number of results if filter activated

--- frontend/components/SearchFilters/PublicationFilters.js
+++ frontend/components/SearchFilters/PublicationFilters.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import uniqBy from 'lodash/uniqBy';
+import some from 'lodash/some';
+import slugify from 'slugify';
+
+import sanityClient from '../../helpers/sanity-client-config';
+
+const findPublications = results => results.filter(({ _type }) => _type === 'publication');
 
 /**
  * Find unique publication types in the list of current search results
@@ -7,28 +13,65 @@ import uniqBy from 'lodash/uniqBy';
  * @return {Array}         publicationTypes
  */
 const findPublicationTypes = (results) => {
-  const publications = results.filter(({ _type }) => _type === 'publication');
+  const publications = findPublications(results);
   return (
     // create a publication list with unique publication titles
     uniqBy(publications, ({ publicationType = {} }) => publicationType.title)
       // create list of only publication title
-      .map(({ publicationType = {} }) => publicationType.title)
+      .map(({ publicationType = {} }) => ({
+        title: publicationType.title,
+        _id: publicationType._id,
+      }))
       // remove publicationType with no title
-      .filter(title => !!title)
+      .filter(({ title }) => !!title)
   );
 };
 
-const CheckBox = publicationTypeTitle => (
-  <div>
-    <label htmlFor={publicationTypeTitle}>
-      <input type="checkbox" name={publicationTypeTitle} value="value" />
-      {publicationTypeTitle}
-    </label>
-  </div>
-);
+const CheckBox = ({ publicationType, disabled = false, results = [] }) => {
+  const numResultsIfFiltered = findPublications(results).filter(
+    resPub => resPub.publicationType._id === publicationType._id,
+  ).length;
+  return (
+    <div>
+      <label htmlFor={publicationType.title}>
+        <input
+          disabled={disabled}
+          type="checkbox"
+          name={publicationType.title}
+          value={slugify(publicationType.title, { lower: true })}
+        />
+        {publicationType.title} ({numResultsIfFiltered})
+      </label>
+    </div>
+  );
+};
 
-const PublicationFilters = ({ results = [] }) => (
-  <div>{findPublicationTypes(results).map(CheckBox)}</div>
-);
+class PublicationFilters extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { allPublicationTypes: [] };
+  }
+  async componentDidMount() {
+    const allPublicationTypes = await sanityClient.fetch('*[_type in ["publicationType"]]');
+    this.setState({ allPublicationTypes });
+  }
+  render() {
+    const { results = [] } = this.props;
+    const publicationTypesInResults = findPublicationTypes(results);
+    return (
+      <div>
+        {this.state.allPublicationTypes.length === 0 && <span>Loading ...</span>}
+        {this.state.allPublicationTypes.map(({ _id, title }) => (
+          <CheckBox
+            key={_id}
+            publicationType={{ _id, title }}
+            results={results}
+            disabled={!some(publicationTypesInResults, resultPub => resultPub._id === _id)}
+          />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default PublicationFilters;
