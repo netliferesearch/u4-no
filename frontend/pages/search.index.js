150212ade15c1645f1b13ad5dd2f8edeee462330
maris <mariselnes@users.noreply.github.com>
2017-10-27 14:03:03 +0200

    adjust search layout

--- frontend/pages/search.index.js
+++ frontend/pages/search.index.js
@@ -38,20 +38,20 @@ function handleChange(query) {}
 
 const Search = ({ results = false }) => (
   <Layout>
-    <div className="o-layout">
-      <div className="o-layout__item u-1/1 o-layout c-search__wrapper">
+
+      <div className="o-layout c-search__wrapper">
         <div className="o-layout__item u-6/12 u-push-3/12">
           <section {...classes({ block: 'search-input', element: 'content' })}>
             <SearchField />
           </section>
         </div>
       </div>
-
+<div className="o-layout">
       <section className="o-layout__item u-6/12 u-push-3/12">
         {results && <SearchResults results={results.filter(item => item.slug)} />}
       </section>
 
-      <section className="o-layout__item u-2/12 u-push-2/12">
+      <section className="o-layout__item u-2/12 u-push-3/12">
         <SearchFilters results={results} />
       </section>
     </div>
