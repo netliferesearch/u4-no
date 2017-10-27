0d8917e886a57f4b66565a257daf552824610f99
kmelve <knut@netliferesearch.com>
2017-10-27 17:23:57 +0200

    add linkable author names

--- frontend/components/AuthorList.js
+++ frontend/components/AuthorList.js
@@ -3,11 +3,11 @@ import React from 'react';
 const AuthorList = ({ authors = [] }) => (
   <span>
     By{' '}
-    {authors.map(({ firstName = '', surname = '', _id = '' }, index) => (
+    {authors.map(({ firstName = '', surname = '', _id = '', slug = false }, index) => (
       <span key={_id}>
-        <a href>
+        {slug ? <a href={`/persons/${slug.current}`}>
           {firstName} {surname}
-        </a>
+        </a> : `${firstName} ${surname}` }
         {/* Add ampersands between the authors. */}
         {authors.length > 1 && index + 1 !== authors.length && <span> & </span>}
       </span>
