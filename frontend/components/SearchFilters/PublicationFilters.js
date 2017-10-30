c283a1ff5b799960dc5f73869c3a44b729445d83
thomaslgj <thomas@netliferesearch.com>
2017-10-30 14:19:29 +0100

    Fix bug in filters

--- frontend/components/SearchFilters/PublicationFilters.js
+++ frontend/components/SearchFilters/PublicationFilters.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import some from 'lodash/some';
 
 import slugify from 'slugify';
-import BEMHelper from 'react-bem-helper';
 
 import sanityClient from '../../helpers/sanity-client-config';
 
 import { findPublicationTypes, findPublications } from './filterHelpers';
 import FilterCheckBox from './FilterCheckBox';
+import BEMHelper from 'react-bem-helper';
 
 const classes = BEMHelper({
   name: 'filters',
   prefix: 'c-',
 });
-
 
 class PublicationFilters extends Component {
   constructor(props) {
@@ -30,22 +29,22 @@ class PublicationFilters extends Component {
     return (
       <div {...classes('item')}>
         {this.state.allPublicationTypes.length === 0 && <span>Loading ...</span>}
-        {this.state.allPublicationTypes &&
-           this.state.allPublicationTypes.map((pub = {}) => (
-             <FilterCheckBox
-               key={pub._id}
-               id={slugify(`pub-type-${pub.title}`, { lower: true })}
-               title={pub.title}
-               {...classes('checkbox')}
-               results={results}
-               numResultsIfFiltered={
-                 findPublications(results).filter(resPub => resPub.publicationType._id === pub._id)
-                   .length
-               }
-               {...this.props}
-               disabled={!some(publicationTypesInResults, resultPub => resultPub._id === pub._id)}
-             />
-           ))}
+        {this.state.allPublicationTypes.map((pub = {}) => (
+          <FilterCheckBox
+            key={pub._id}
+            id={slugify(`pub-type-${pub._id}`, { lower: true })}
+            title={pub.title}
+            {...classes('checkbox')}
+            results={results}
+            numResultsIfFiltered={
+              findPublications(results).filter(
+                ({ publicationType = {} }) => publicationType._id === pub._id,
+              ).length
+            }
+            {...this.props}
+            disabled={!some(publicationTypesInResults, resultPub => resultPub._id === pub._id)}
+          />
+        ))}
       </div>
     );
   }
