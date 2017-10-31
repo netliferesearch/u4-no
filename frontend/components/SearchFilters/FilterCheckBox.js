67f83f51a34eaa4d8461a5caf458989a8bccefcc
Nils Norman Haukås <nils@thunki.com>
2017-10-31 09:42:01 +0100

    gst

--- frontend/components/SearchFilters/FilterCheckBox.js
+++ frontend/components/SearchFilters/FilterCheckBox.js
@@ -2,6 +2,17 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { addSearchFilter, removeSearchFilter } from '../../helpers/redux-store';
 
+/**
+ *  This checkbox is responsible for adding/removing a filter in the redux store.
+ *  Furthermore it has no concept of what is filtering be it publication or topic.
+ *
+ * @param {String}  [id='']             [description]
+ * @param {String}  [title='']          [description]
+ * @param {String}  [className='']      [description]
+ * @param {Boolean} [disabled=false]    [description]
+ * @param {Array}   [results=[]]        [description]
+ * @param {[type]}  [addSearchFilter=(] [description]
+ */
 const FilterCheckBox = ({
   id = '',
   title = '',
