416a63153fa995435c39da7c0b42b90650530cd6
Nils Norman Haukås <nils@thunki.com>
2017-10-31 14:36:29 +0100

    bugfix buildTitleObjects logic, and comment it for clarity.

--- frontend/components/TableOfContents/buildTitleObjects.js
+++ frontend/components/TableOfContents/buildTitleObjects.js
@@ -9,17 +9,36 @@ const buildTitleObject = (elem) => {
   };
 };
 
+/**
+ * Loops through all titles in a sanity content block and creates a list of
+ * objects like so:
+ *
+ * [{
+ *  title: 'Large header',
+ *  style: 'h2',
+ *  id: 'large-header',
+ *  children: [{
+ *    title: 'Sub Heading',
+ *    style: 'h3',
+ *    id: 'sub-heading'
+ *  }]
+ * }]
+ */
 const buildTitleObjects = (content = []) =>
-  content.reduce((result, elem) => {
-    const lastResultItem = result[result.length - 1];
+  content.reduce((titles, elem) => {
+    const previousTitle = titles[titles.length - 1];
     if (elem.style === 'h2') {
-      result.push(buildTitleObject(elem));
-    } else if (elem.style === 'h3' && lastResultItem.children) {
-      lastResultItem.children.push(buildTitleObject(elem));
-    } else if (elem.style === 'h3') {
-      lastResultItem.children = [buildTitleObject(elem)];
+      titles.push(buildTitleObject(elem));
+    } else if (elem.style === 'h3' && !previousTitle) {
+      // if the article begins with an h3 but there are no previous titles
+      // we then promote it to an h2
+      titles.push(buildTitleObject(elem));
+    } else if (elem.style === 'h3' && previousTitle.children) {
+      previousTitle.children.push(buildTitleObject(elem));
+    } else if (elem.style === 'h3' && previousTitle) {
+      previousTitle.children = [buildTitleObject(elem)];
     }
-    return result;
+    return titles;
   }, []);
 
 export default buildTitleObjects;
