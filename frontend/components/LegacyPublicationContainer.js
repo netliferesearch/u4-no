fdb9fe6ef9678a6608d7bacf826b3c62fc0e2a61
Nils Norman Haukås <nils@thunki.com>
2017-11-02 11:25:02 +0100

    display legacyPublication abstract as lead. Use publication header

--- frontend/components/LegacyPublicationContainer.js
+++ frontend/components/LegacyPublicationContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Document } from 'react-pdf';
+import { Document } from 'react-pdf/build/entry.noworker';
 import { toggleArticleMenu, toggleLoadingScreen } from '../helpers/redux-store';
 import {
   Footer,
@@ -23,6 +23,7 @@ const LegacyPublicationContainer = (props) => {
     isArticleMenuOpen,
     showLoadingScreen,
     lead = '',
+    abstract,
     _type = '',
     longTitle = '',
     title = '',
@@ -34,118 +35,58 @@ const LegacyPublicationContainer = (props) => {
   } = props;
   return (
     <Layout showLoadingScreen={showLoadingScreen} showTopTab={!isArticleMenuOpen}>
-      {isArticleMenuOpen && (
-        <div
-          className={`c-article-nav-fullscreen ${isArticleMenuOpen
-            ? 'c-article-nav-fullscreen c-article-nav-fullscreen--open'
-            : ''}`}
-        >
-          <TableOfContentsButton {...props} />
-          <TableOfContentsContent
-            showAllItems
-            onItemSelected={(e) => {
-              const linkThatWasClicked = e.target;
-              // prevent this from triggering multiple times
-              if (!showLoadingScreen) {
-                toggleLoadingScreen();
-                setTimeout(() => {
-                  toggleArticleMenu();
-                  linkThatWasClicked.click();
-                  setTimeout(() => {
-                    toggleLoadingScreen();
-                  }, 1);
-                }, 1);
-              }
-            }}
-            {...props}
-          />
+      <article className="u-relative">
+        {BreadCrumbComponent && BreadCrumbComponent}
+
+        <div className="c-hero">
+          {props.featuredImage &&
+            props.featuredImage.asset.url && (
+              <div
+                className="c-hero-image"
+                style={{
+                  backgroundImage: `url(${props.featuredImage.asset.url})`,
+                }}
+              />
+            )}
+          <div className="c-hero-bg" />
+          <div className="c-hero-sideText">
+            {props.featuredImage && props.featuredImage.credit ? (
+              <a href={props.featuredImage.sourceUrl}>{props.featuredImage.credit}</a>
+            ) : null}
+          </div>
+          <div className="c-hero-header">
+            <PublicationArticleHeader
+              className="c-hero__grid-container__content links-wrapper-dark-background"
+              {...props}
+            />
+          </div>
         </div>
-      )}
-      {!isArticleMenuOpen && (
-        <article className="u-relative">
-          {_type === 'publication' &&
-            !isPublicationDrawerOpen && <TableOfContentsButton {...props} />}
-          <CustomScrollSpy {...props} />
-          <span id="js-top" />
-          <div id="js-scroll-trigger">
-            {BreadCrumbComponent && BreadCrumbComponent}
-            {_type === 'publication' &&
-              props.featuredImage &&
-              props.featuredImage.asset.url && (
-                <div className="c-hero">
-                  <div
-                    className="c-hero-image"
-                    style={{
-                      backgroundImage: `url(${props.featuredImage.asset.url})`,
-                    }}
-                  />
-                  <div className="c-hero-bg" />
-                  <div className="c-hero-sideText">
-                    {props.featuredImage.credit ? (
-                      <a href={props.featuredImage.sourceUrl}>{props.featuredImage.credit}</a>
-                    ) : null}
-                  </div>
-                  <div className="c-hero-header">
-                    <PublicationArticleHeader
-                      className="c-hero__grid-container__content links-wrapper-dark-background"
-                      {...props}
-                    />
-                  </div>
-                </div>
+        <div className="c-longform-grid">
+          <div className="c-longform-grid__standard">
+            {lead && (
+              <div className="c-article">
+                <p>{lead}</p>
+              </div>
+            )}
+            {/* Legacy publication abstracts come with html included
+                so we go and render it out.
+               */}
+            {!lead &&
+              abstract && (
+                <div className="c-article" dangerouslySetInnerHTML={{ __html: abstract }} />
               )}
           </div>
-          {_type === 'publication' && (
-            <div className="c-longform-grid">
-              <div className="c-longform-grid__standard">
-                {lead && (
-                  <div className="c-article">
-                    <p>{lead}</p>
-                  </div>
-                )}
-                {mainPoints.length > 0 && (
-                  <div className="c-article c-article_mainPoints">
-                    <h2>Main points</h2>
-                    <ul className="c-article_mainPoints-list">
-                      {mainPoints.map((mainPoint, index) => (
-                        <li key={index} className="c-article_mainPoints-item">
-                          <span className="c-article_mainPoints-firstWords">
-                            {mainPoint
-                              .split(' ')
-                              .slice(0, 3)
-                              .join(' ')}{' '}
-                          </span>
-                          <span className="c-article_mainPoints-lastWords">
-                            {mainPoint
-                              .split(' ')
-                              .slice(3)
-                              .join(' ')}
-                          </span>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                )}
-              </div>
-              {_type === 'publication' &&
-                !isPublicationDrawerOpen && (
-                  <div className="c-longform-grid__sidebar-right">
-                    <TableOfContentsSidebar {...props} />
-                  </div>
-                )}
-            </div>
-          )}
-          {_type !== 'publication' && (
-            <div className="c-longform-grid">
-              <h1 className="c-longform-grid__standard">{longTitle || title}</h1>
-              {lead && <div className="c-article c-longform-grid__standard">{lead}</div>}
-              <div className="c-longform-grid__sidebar-right">
-                <TableOfContentsSidebar alwaysFollow {...props} />
-              </div>
-            </div>
-          )}
-          {legacypdf.asset && <Document file={{ url: legacypdf.asset.url }} />}
-        </article>
-      )}
+        </div>
+        {legacypdf.asset && (
+          <div className="c-longform-grid__larger">
+            <Document
+              file={{
+                url: legacypdf.asset.url,
+              }}
+            />
+          </div>
+        )}
+      </article>
     </Layout>
   );
 };
