9881f0abf6045c4746dce053290dc3e4c17740f3
Nils Norman Haukås <nils@thunki.com>
2017-11-02 15:04:27 +0100

    make pdf viewer responsive by listening for resize events triggering re-renders. Commented this

--- frontend/components/PdfViewer.js
+++ frontend/components/PdfViewer.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 import autoBind from 'react-autobind';
 import { Document, Page } from 'react-pdf';
+import throttle from 'lodash/throttle';
 
+/**
+ * PDF viewer that uses Mozillas tried and true pdf viewer underneath.
+ *
+ * This component listens for resize events and triggers re-renders of the pdf.
+ * This is because the rendered pdf element cannot be simply resized with css.
+ *
+ * TODO: The react-pdf library has potential for leveraging service workers for rendering.
+ */
 export default class PdfViewer extends Component {
   constructor(props) {
     super(props);
+    autoBind(this);
     this.state = {
       numPages: null,
       pageNumber: 1,
+      pdfWidth: 700,
+      // resizing a browser triggers a ton of events this is to
+      // prevent it from nedlessly triggering re-renders
+      resizeHandler: throttle(() => this.updateWindowWidth(), 3000),
     };
-    autoBind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.keyPressHandler);
+    window.addEventListener('resize', this.state.resizeHandler);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.keyPressHandler);
+    window.removeEventListener('resize', this.state.resizeHandler);
   }
 
   onDocumentLoad({ numPages }) {
-    this.setState({ numPages });
+    this.setState({ numPages }, this.updateWindowWidth);
+  }
+
+  keyPressHandler(e) {
+    const left = 37;
+    const right = 39;
+    if (left === e.keyCode) {
+      this.decrementPageNumber();
+    } else if (right === e.keyCode) {
+      this.incrementPageNumber();
+    }
+  }
+
+  updateWindowWidth() {
+    if (typeof window === 'undefined') {
+      // do nothing
+    } else {
+      const windowWidth = window.innerWidth;
+      // let's make the pdf slightly narrower than the fullwidth window
+      this.setState({ pdfWidth: windowWidth - 50 });
+    }
   }
 
   incrementPageNumber() {
@@ -33,9 +76,8 @@ export default class PdfViewer extends Component {
   }
 
   render() {
-    const { pageNumber, numPages } = this.state;
+    const { pageNumber, numPages, pdfWidth } = this.state;
     const { file } = this.props;
-
     return (
       <div className="c-pdf-viewer">
         <div className="c-pdf-viewer__controls">
@@ -55,7 +97,7 @@ export default class PdfViewer extends Component {
         </div>
         <div className="c-pdf-viewer__pdf">
           <Document file={file} onLoadSuccess={this.onDocumentLoad}>
-            <Page scale="1.5" pageNumber={pageNumber} />
+            <Page width={pdfWidth} pageNumber={pageNumber} />
           </Document>
         </div>
       </div>
