aacf522af3decb46dfeba837933a779cde160024
kmelve <knut@netliferesearch.com>
2017-11-02 10:13:59 +0100

    use spinner

--- backend/plugins/FunkyWorkflow/src/FunkyWorkFlow.js
+++ backend/plugins/FunkyWorkflow/src/FunkyWorkFlow.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { route } from 'part:@sanity/base/router'
-import Icon from './components/Icon'
 import client from 'part:@sanity/base/client'
+import Spinner from 'part:@sanity/components/loading/spinner'
+import Preview from 'part:@sanity/components/previews/default'
+import Icon from './components/Icon'
 
 
 
@@ -11,9 +13,6 @@ const Pane = ({ data = false }) => {
     {
         data && data.map(({_id, _type, title, workflow}, index) => <li key={index}>
           <h2><a href={`/desk/${_type}/edit/${_id}`}>{title}</a></h2>
-          {
-            console.log(workflow)
-          }
           <h3>Progress: {workflow.progress}</h3>
           <h3>Assigned to: {workflow.assigned.map(({ firstName, surname, _type, _id }, index) => <a key={index} href={`/desk/${_type}/edit/${_id}`}>{firstName} {surname}</a>)}</h3>
         </li>)
@@ -38,7 +37,7 @@ class FunkyWorkflow extends Component {
     this.setState({ data, loaded: true })
   }
   render() {
-    if (!this.state.loaded) return <div>Loading…</div>
+    if (!this.state.loaded) return <Spinner fullscreen center message="Loading documents" />
     return (<div>
       <Pane data={this.state.data} />
     </div>)
