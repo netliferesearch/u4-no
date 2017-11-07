92dcfd82d2d4a04104b55ffe6bd4dfc866ca424e
thomaslgj <thomas@netliferesearch.com>
2017-11-07 15:13:11 +0100

    New styling for persons page

--- frontend/pages/persons.index.js
+++ frontend/pages/persons.index.js
@@ -1,31 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import sortBy from 'lodash/sortBy';
 import { Link } from '../routes';
 import { Layout, Footer, Team } from '../components';
 import BreadCrumb from '../components/BreadCrumb';
 import DataLoader from '../helpers/data-loader';
 
-const Persons = ({ frontpage = {}, persons = [], url }) => (
-  <Layout>
-    <h1 className="c-article__title">{frontpage.title}</h1>
-    <BreadCrumb url={url} />
-    <div className="c-article__lead">{frontpage.lead}</div>
-    <div className="c-topic-section--lightblue">
-      <div className="o-wrapper c-article">
-        <div id="advisors">
-          <Team light members={sortBy(persons, ({ firstName = '' }) => firstName.toUpperCase())} />
+
+class Persons extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeGroup: 1,
+    };
+    this.changeGroup = this.changeGroup.bind(this);
+  }
+
+  changeGroup(group) {
+    this.setState({
+      activeGroup: group,
+    });
+  }
+
+  render() {
+    const { frontpage, persons, helpdesk, url } = this.props;
+    return (
+      <Layout>
+        <h1 className="c-article__title c-article__title--center">{frontpage.title}</h1>
+        <BreadCrumb url={url} />
+        <div className="c-article__lead c-article__lead--center">{frontpage.lead}</div>
+        <div className="c-filters--standalone">
+          <div className="c-filters__label--standalone">Filter people by
+            <button className="c-filters__item--standalone" onClick={() => this.changeGroup(1)}><a>U4 Team</a></button>
+            <button className="c-filters__item--standalone" onClick={() => this.changeGroup(2)}><a>U4 Helpdesk</a></button>
+          </div>
         </div>
-      </div>
-    </div>
-    <Footer />
-  </Layout>
-);
+        <div className="o-wrapper c-article">
+          {
+            this.state.activeGroup == 1 ?
+              <div id="advisors">
+                <h2 className="c-article__title c-article__title--center">U4 TEAM</h2>
+                <Team light members={sortBy(persons, ({ firstName = '' }) => firstName.toUpperCase())} />
+              </div>
+              : null
+          }
+          {
+            this.state.activeGroup == 2 ?
+              <div id="advisors">
+                <h2 className="c-article__title c-article__title--center">U4 HELPDESK</h2>
+                <Team light members={sortBy(helpdesk, ({ firstName = '' }) => firstName.toUpperCase())} />
+              </div>
+              : null
+          }
+        </div>
+        <Footer />
+      </Layout>
+    );
+  }
+}
 
 export default DataLoader(Persons, {
   queryFunc: ({ query: { slug = '' } }) => ({
     sanityQuery: `{
       "frontpage": *[_id == "627b8d42-d8f7-4cf6-9567-f6337678b688"][0],
       "persons": *[_type == "person" && references("0e1b0ebc-e016-4d2b-97e8-859b91e3e147") && "0e1b0ebc-e016-4d2b-97e8-859b91e3e147" in affiliations[]._ref][0..100]{..., "image": image.asset->url[0], affiliations},
+      "helpdesk": *[_type == "person" && references("17ec3576-0afa-4203-9626-a38a16b27c2a") && "17ec3576-0afa-4203-9626-a38a16b27c2a" in affiliations[]._ref][0..100]{..., "image": image.asset->url[0], affiliations},
     }`,
   }),
   materializeDepth: 2,
