42f744bc270883313f91c0d82955a4798c93bca3
thomaslgj <thomas@netliferesearch.com>
2017-11-08 15:59:06 +0100

    Add pages for frontpage types

--- frontend/components/ServiceArticle.js
+++ frontend/components/ServiceArticle.js
@@ -54,11 +54,50 @@ const serializers = {
         </div>
       </section>
     ),
+    block: ({ node, children }) => {
+      const style = node.style || 'normal';
+
+      // Heading?
+      if (/^h\d/.test(style)) {
+        const level = parseInt(style.slice(1), 10);
+        const id = level === 2 || level === 3
+          ? slugify(children[0], { lower: true })
+          : undefined;
+
+        return React.createElement(
+          style,
+          { id, className: 'o-grid-container__item-standard' },
+          children,
+        );
+      }
+
+      if (style === 'blockquote') {
+        return (
+          <blockquote className="o-grid-container__item-standard">
+            {children}
+          </blockquote>
+        );
+      }
+
+      return (
+        <p className="o-grid-container__item-standard">
+          {children}
+        </p>
+      );
+    },
   },
 };
 
-const ServiceArticle = ({ blocks }) => (
-  <BlockContent blocks={blocks} serializers={serializers} />
-);
+const ServiceArticle = ({ blocks }) =>
+  // const newkid = blocks.filter(block => !['reference'].includes(block._type));
+  //
+  // if (!newkid.length) {
+  //   return null;
+  // }
+
+  (
+    <BlockContent blocks={blocks} serializers={serializers} />
+  )
+;
 
 export default ServiceArticle;
---
+++ frontend/pages/service.workshops.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { Link } from '../routes';
+import sanityClient from '@sanity/client';
+import DataLoader from '../helpers/data-loader';
+import Head from 'next/head';
+
+import { BoxOnBox, Footer, Layout, ExtendedBlockContent, Accordion, Newsletter, ServiceArticle } from '../components';
+import { Feature, Mosaic, LinkBox } from '../components';
+import { DownArrowButton, RightArrowButton } from '../components/buttons';
+import { Basics, Picture, Publication, Resources, ResearchAgenda, ArrowRight } from '../components/icons';
+
+const ServicePage = ({
+  service = {},
+}) => (
+  <Layout>
+    <h2
+      className="c-topic-page_title"
+    >
+      {service.title}
+    </h2>
+    <h2
+      className="c-topic-page__longTitle"
+    >
+      {service.longTitle}
+    </h2>
+    {service.featuredImage ?
+      <section
+        className="c-boxOnImage"
+      >
+        <figure
+          className="c-boxOnImage__figure"
+        >
+          <img
+            alt=""
+            src={service.featuredImage}
+          />
+        </figure>
+        <div
+          className="c-boxOnImage__body"
+        >
+          <p
+            className="c-boxOnImage__lead"
+          >
+            We facilitate local dialouge
+          </p>
+          {service.lead.split('\n').map(i => <p>{i}</p>)}
+        </div>
+      </section>
+      : null
+    }
+
+    <ServiceArticle
+      blocks={service.sections}
+    />
+
+    <Newsletter />
+
+    <Footer />
+  </Layout>
+);
+export default DataLoader(ServicePage, {
+  queryFunc: ({ query: { slug = '' } }) => ({
+    sanityQuery: '{ "service": *[slug.current == "workshops-and-events"][0]{title, longTitle, slug, lead, _id, sections, "featuredImage": featuredImage.asset->url}}',
+    param: { slug },
+  }),
+  materializeDepth: 3,
+});
+
