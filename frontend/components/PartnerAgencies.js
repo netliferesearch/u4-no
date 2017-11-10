e0655f258ace62b19ab871d3f00ea6bbd8bb024e
thomaslgj <thomas@netliferesearch.com>
2017-11-10 12:21:56 +0100

    Fix typo

--- frontend/components/PartnerAgencies.js
+++ frontend/components/PartnerAgencies.js
@@ -12,7 +12,7 @@ const PartnerAgencies = ({ title }) => (
 
   <div {...classes('')}>
     <div {...classes('heading')}>
-      <h1>Partner Agencies</h1>
+      <h1>Partner agencies</h1>
       <p {...classes('text')}>The U4 Anti-Corruption Resource Centre is an institutional partnership
   of bilateral international development agencies/ministries of foreign affairs.</p>
     </div>
