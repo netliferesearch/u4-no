b489c37fd9ce222dafca9c821ee95d5972a89c70
Robert Sjursen <robert.sjursen@cmi.no>
2017-11-10 13:06:35 +0100

    Add category field to keyword (will replace iscountry field)

--- backend/schemas/keyword.js
+++ backend/schemas/keyword.js
@@ -4,13 +4,27 @@ export default {
   fields: [
     {
       name: 'keyword',
+      description: 'All keywords must: - make sense as stand-alone words - be in English - be a feasible search term - likely apply to several resources',
       type: 'string'
     },
     {
       title: 'Does this keyword represent a country?',
       name: 'isCountry',
       type: 'boolean'
-    }
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "string",
+      options: {
+        list: [
+          { title: 'Keyword', value: 'keyword'},
+          { title: 'Country', value: 'country'},
+          { title: 'Region', value: 'region'},
+        ],
+      },
+      layout: "dropdown",
+    },
   ],
   preview: {
     select: {
