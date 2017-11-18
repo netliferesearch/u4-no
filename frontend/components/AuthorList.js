ec83ff05b1df6107583340956ecb17009f480f16
Robert Sjursen <robert.sjursen@cmi.no>
2017-11-18 09:17:20 +0100

    Use target if defined in AuthorList

--- frontend/components/AuthorList.js
+++ frontend/components/AuthorList.js
@@ -6,6 +6,7 @@ const AuthorList = ({ authors = false }) => (
     {authors &&
       authors
         .filter(author => author)
+        .map(author => (author.target ? author.target : author))
         .map(({ firstName = '', surname = '', _id = '', slug = false }, index) => (
           <span key={_id}>
             {slug ? (
@@ -20,6 +21,7 @@ const AuthorList = ({ authors = false }) => (
             {authors.length > 1 && index + 2 === authors.length && <span> and </span>}
           </span>
         ))}
+    {console.log(authors)}
   </span>
 );
 
