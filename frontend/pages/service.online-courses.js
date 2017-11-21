96ab78fdde4e42458adf7e8af3ffe3a72de66b65
Robert Sjursen <robert.sjursen@cmi.no>
2017-11-21 16:54:49 +0100

    Change query so service.online-courses gets data from frontpage

--- frontend/pages/service.online-courses.js
+++ frontend/pages/service.online-courses.js
@@ -5,50 +5,43 @@ import DataLoader from '../helpers/data-loader';
 import Head from 'next/head';
 import BlockContent from '@sanity/block-content-to-react';
 
-import { BoxOnBox, Footer, Layout, ExtendedBlockContent, Accordion, Newsletter, ServiceArticle } from '../components';
+import {
+  BoxOnBox,
+  Footer,
+  Layout,
+  ExtendedBlockContent,
+  Accordion,
+  Newsletter,
+  ServiceArticle,
+} from '../components';
 import { Feature, Mosaic, LinkBox, LinkList } from '../components';
 import { DownArrowButton, RightArrowButton } from '../components/buttons';
-import { Basics, Picture, Publication, Resources, ResearchAgenda, ArrowRight } from '../components/icons';
+import {
+  Basics,
+  Picture,
+  Publication,
+  Resources,
+  ResearchAgenda,
+  ArrowRight,
+} from '../components/icons';
 
-const ServicePage = ({
-  service = {},
-}) => (
+const ServicePage = ({ service = {} }) => (
   <Layout>
-    <h2
-      className="c-topic-page_title"
-    >
-      {service.title}
-    </h2>
-    <h2
-      className="c-topic-page__longTitle"
-    >
-      {service.longTitle}
-    </h2>
-    {service.featuredImage ?
-      <section
-        className="c-boxOnImage"
-      >
-        <figure
-          className="c-boxOnImage__figure"
-        >
-          <img
-            alt=""
-            src={service.featuredImage}
-          />
+    <h2 className="c-topic-page_title">{service.title}</h2>
+    <h2 className="c-topic-page__longTitle">{service.longTitle}</h2>
+    {service.featuredImage ? (
+      <section className="c-boxOnImage">
+        <figure className="c-boxOnImage__figure">
+          <img alt="" src={service.featuredImage} />
         </figure>
-        <div
-          className="c-boxOnImage__body"
-        >
+        <div className="c-boxOnImage__body">
           <BlockContent blocks={service.lead} />
           {service.leadLinks && <LinkList title="" content={service.leadLinks} />}
         </div>
       </section>
-      : null
-    }
+    ) : null}
 
-    <ServiceArticle
-      blocks={service.sections}
-    />
+    <ServiceArticle blocks={service.sections} />
 
     <Newsletter />
 
@@ -57,9 +50,9 @@ const ServicePage = ({
 );
 export default DataLoader(ServicePage, {
   queryFunc: ({ query: { slug = '' } }) => ({
-    sanityQuery: '{ "service": *[slug.current == "online-courses"][0]{title, longTitle, slug, lead, leadLinks, _id, sections, "featuredImage": featuredImage.asset->url}}',
+    sanityQuery:
+      '{ "service": *[_type=="frontpage" && slug.current == "online-courses"][0]{title, longTitle, slug, lead, leadLinks, _id, sections, "featuredImage": featuredImage.asset->url}}',
     param: { slug },
   }),
   materializeDepth: 2,
 });
-
