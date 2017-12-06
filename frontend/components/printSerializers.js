9ee1242e486b33fe6e48a9f5129696902c7f423f
Knut Melvær <knut.melvaer@netlife.com>
2017-12-06 08:37:47 +0100

    add footnotes to print version

--- frontend/components/printSerializers.js
+++ frontend/components/printSerializers.js
@@ -1,90 +1,132 @@
 import BEMHelper from 'react-bem-helper';
 import BlockContent from '@sanity/block-content-to-react';
+import slugify from 'slugify';
 import FunkyTable from './FunkyTable';
 import LineChart from './LineChart';
 import BarChart from './BarChart';
+import findFootnotes from './findFootnotes';
+import { PullQuote, Figure } from './';
 
 const classes = BEMHelper({
   name: 'longform-grid',
   prefix: 'c-',
 });
-
-import slugify from 'slugify';
-import { PullQuote, Figure } from './';
-
-export default {
+const footnoteSerializer = {
   types: {
-    image: ({ node }) => <Figure {...node} />,
-    pullQuote: ({ node: { text } }) => (
-      <div {...classes('medium')}>
-        <PullQuote>{text}</PullQuote>
-      </div>
-    ),
-    nugget: ({ node: { text, title } }) => (
-      <div {...classes('standard')}>
-        <div {...classes('nugget')}>
-          <h2 {...classes('nugget-title')}>{title}</h2>
-          <BlockContent blocks={text} />
-        </div>
-      </div>
-    ),
-    funkyTable: ({ node: { display = 'table', rows = [], title = false } }) => {
-      if (display === 'line') return <LineChart rows={rows} title={title} />;
-      if (display === 'bar') return <BarChart rows={rows} title={title} />;
-      return <FunkyTable rows={rows} title={title} />;
-    },
     block: ({ node, children }) => {
       const style = node.style || 'normal';
-
-      // Heading?
-      if (/^h\d/.test(style)) {
-        const level = parseInt(style.slice(1), 10);
-        const id = level === 2 || level === 3 ? slugify(children[0], { lower: true }) : undefined;
-
-        return React.createElement(style, { id, className: 'c-longform-grid__standard' }, children);
-      }
-
-      if (style === 'blockquote') {
-        return <div {...classes('large-right')}><blockquote>{children}</blockquote></div>;
-      }
-
-      return <p {...classes('standard')}>{children}</p>;
-    },
-  },
-
-  list: ({ type, children }) => {
-    if (type === 'bullet') {
-      return <ul {...classes('standard', null, 'list-bullets')}>{children}</ul>;
-    }
-
-    return <ol {...classes('standard', null, 'list-numbered')}>{children}</ol>;
-  },
-  marks: {
-    internalReferance: props => <a href={props.mark.href}>{props.children}</a>,
-    link: (props) => {
-      if (props.mark.href) {
-        if (props.mark.href.match(/#_ftn(\d+)/)) {
-          const ref = props.mark.href.match(/#_ftn(\d+)/)[1];
-          return (<span className="fn" id={`fnref:${ref}`}>
-            This is a footnote!
-          </span>);
-        }
-        if (props.mark.href.match(/#_ftnref(\d+)/)) {
-          const ref = props.mark.href.match(/#_ftnref(\d+)/)[1];
-          return (<li className="c-footnote__item" id={`fn:${ref}`}>
-            {props.children}<a href={`#fnref:${ref}`} title="return to article"> ↩</a>
-          </li>);
-        }
-        return <span>{props.children}<span className="fn"><a href={props.mark.href} /></span></span>;
-      }
-
-      console.log(props.mark);
-
-      return null;
-    },
-    footnote: (props) => {
-      console.log('footnote', props);
-      return <span className="fn">{props.children}</span>;
+      return <span>{children}</span>;
     },
   },
 };
+
+function printSerializers(blocks) {
+  const footnotes = findFootnotes(blocks);
+  return {
+    types: {
+      image: ({ node }) => <Figure {...node} />,
+      pullQuote: ({ node: { text } }) => (
+        <div {...classes('medium')}>
+          <PullQuote>{text}</PullQuote>
+        </div>
+      ),
+      nugget: ({ node: { text, title } }) => (
+        <div {...classes('standard')}>
+          <div {...classes('nugget')}>
+            <h2 {...classes('nugget-title')}>{title}</h2>
+            <BlockContent blocks={text} />
+          </div>
+        </div>
+      ),
+      funkyTable: ({ node: { display = 'table', rows = [], title = false } }) => {
+        if (display === 'line') return <LineChart rows={rows} title={title} />;
+        if (display === 'bar') return <BarChart rows={rows} title={title} />;
+        return <FunkyTable rows={rows} title={title} />;
+      },
+      block: ({ node, children }) => {
+        const style = node.style || 'normal';
+
+        // Heading?
+        if (/^h\d/.test(style)) {
+          const level = parseInt(style.slice(1), 10);
+          const id = level === 2 || level === 3 ? slugify(children[0], { lower: true }) : undefined;
+
+          return React.createElement(
+            style,
+            { id, className: 'c-longform-grid__standard' },
+            children,
+          );
+        }
+
+        if (style === 'blockquote') {
+          return (
+            <div {...classes('large-right')}>
+              <blockquote>{children}</blockquote>
+            </div>
+          );
+        }
+
+        return <p {...classes('standard')}>{children}</p>;
+      },
+    },
+
+    list: ({ type, children }) => {
+      if (type === 'bullet') {
+        return <ul {...classes('standard', null, 'list-bullets')}>{children}</ul>;
+      }
+
+      return <ol {...classes('standard', null, 'list-numbered')}>{children}</ol>;
+    },
+    marks: {
+      internalReferance: props => <a href={props.mark.href}>{props.children}</a>,
+      link: (props) => {
+        if (props.mark.href) {
+          if (props.mark.href.match(/#_ftn(\d+)/)) {
+            const ref = props.mark.href.match(/#_ftn(\d+)/)[1];
+            return (
+              <span className="fn" id={`fnref:${ref}`}>
+                This is a footnote!
+              </span>
+            );
+          }
+          if (props.mark.href.match(/#_ftnref(\d+)/)) {
+            const ref = props.mark.href.match(/#_ftnref(\d+)/)[1];
+            return (
+              <li className="c-footnote__item" id={`fn:${ref}`}>
+                {props.children}
+                <a href={`#fnref:${ref}`} title="return to article">
+                  {' '}
+                  ↩
+                </a>
+              </li>
+            );
+          }
+          return (
+            <span>
+              {props.children}
+              {/*             <span className="fn">
+              <a href={props.mark.href} />
+            </span> */}
+            </span>
+          );
+        }
+
+        console.log(props.mark);
+
+        return null;
+      },
+      footnote: ({ children, markKey = '' }) =>(
+        <span>
+          {children}
+          <span className="fn">
+            {markKey && (
+            <BlockContent blocks={footnotes[markKey]} serializers={footnoteSerializer} />
+              )}
+          </span>
+        </span>
+      ),
+    },
+  };
+}
+
+export default printSerializers;
