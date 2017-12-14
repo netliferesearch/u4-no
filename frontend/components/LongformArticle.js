fa66e3542e3fbdfc5ca7ecea7d593f0d7d9b3c02
Knut Melvær <knut.melvaer@netlife.com>
2017-12-14 17:11:05 +0100

    further fixes on footnotes to make them responsive

--- frontend/components/LongformArticle.js
+++ frontend/components/LongformArticle.js
@@ -1,14 +1,8 @@
 import React, { Component } from 'react';
-import BEMHelper from 'react-bem-helper';
 import BlockContent from '@sanity/block-content-to-react';
 import serializers from './serializers';
 import findFootnotes from './findFootnotes';
 import footnoteSerializer from './footnoteSerializer';
-
-const classes = BEMHelper({
-  name: 'longform-grid',
-  prefix: 'c-',
-});
 
 /**
  * Here we replace Sanity's react components for rendering basic things like
@@ -17,29 +11,17 @@ const classes = BEMHelper({
  */
 
 class LongformArticle extends Component {
-  constructor(props) {
-    super(props);
-  }
   componentDidMount() {
-    const littlefoot = require('littlefoot').default;
-    littlefoot(/* {
-      buttonTemplate: `<span class="littlefoot-footnote__container">
-    <button class="littlefoot-footnote__button littlefoot-footnote__button__ellipsis" id="<%= reference %>" data-footnote-content="<%= content %>" data-footnote-id="<%= id %>" data-footnote-number="<%= number %>" alt="See Footnote <%= number %>" aria-controls="fncontent:<%= id %>" aria-expanded="false" aria-label="Footnote <%= number %>" rel="footnote">
-         <span class="littlefoot-footnote__number"><%= number %></span>
-    </button>
-</span>`,
-    } */);
+    this.littlefootActivator();
   }
   componentDidUpdate() {
-    const littlefoot = require('littlefoot').default;
-    littlefoot(/* {
-      buttonTemplate: `<span class="littlefoot-footnote__container">
-    <button class="littlefoot-footnote__button littlefoot-footnote__button__ellipsis" id="<%= reference %>" data-footnote-content="<%= content %>" data-footnote-id="<%= id %>" data-footnote-number="<%= number %>" alt="See Footnote <%= number %>" aria-controls="fncontent:<%= id %>" aria-expanded="false" aria-label="Footnote <%= number %>" rel="footnote">
-         <span class="littlefoot-footnote__number"><%= number %></span>
-    </button>
-</span>`,
-    } */);
+    this.littlefootActivator();
   }
+  littlefootActivator() {
+    const littlefoot = require('littlefoot').default;
+    littlefoot();
+  }
+
   render() {
     const { content = [] } = this.props;
     const blocks = content.filter(block => !['reference'].includes(block._type));
@@ -52,13 +34,14 @@ class LongformArticle extends Component {
         }`}
       >
         <BlockContent blocks={blocks} serializers={serializers} />
-
-        <div className="footnotes">
-          <ol>
-            { footNotesKeys.map(key => (
-              <BlockContent blocks={footnotes[key]} serializers={footnoteSerializer(key)} />
-            ))}
-          </ol>
+        <div>
+          <div className="footnotes">
+            <ol>
+              { footNotesKeys.map(key => (
+                <BlockContent blocks={footnotes[key]} serializers={footnoteSerializer(key)} />
+              ))}
+            </ol>
+          </div>
         </div>
       </main>
     );
--- frontend/components/footnoteSerializer.js
+++ frontend/components/footnoteSerializer.js
@@ -1,11 +1,11 @@
-export default (key = false) => ({
+export default (key = '') => ({
   types: {
     span: ({ node, children }) => {
       console.log(node, children);
       return null
     },
     block: ({ node, children }) => {
-      if (!key) return;
+      if (!key) return null;
       const style = node.style || 'normal';
       return (
         <li className="footnote" id={`fn:${key}`}>
