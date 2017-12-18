2cff5c3aa8ff68b232e8e5d98103099232cd7526
Nils Norman Haukås <nils@thunki.com>
2017-12-18 07:33:23 +0100

    refactor, expand on comments

--- publication-pdf-builder/pdfConfig.js
+++ publication-pdf-builder/pdfConfig.js
@@ -1,33 +1,38 @@
+require('dotenv').config();
+
 /**
  * DocRaptor configuration for an request with axios.
  * Set PDF_TEST to activate test mode in order
  * to prevent using of quota when testing the service.
- * @param {*url} param0
+ *
+ * @param  {String} [url='']        url to page we want to render
+ * @param  {String} [title='output' }]  title of the pdf returned
+ * @return {Object}                 axios request configuration
  */
-require('dotenv').config();
-
-const token = process.env.DOCRAPTOR_API_KEY;
-const pdfConfig = ({ url = '', title = 'output' }) => ({
-  url: `https://${token}@docraptor.com/docs`,
-  method: 'POST',
-  encoding: null, // IMPORTANT! This produces a binary body response instead of text
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  data: {
-    doc: {
-      user_credentials: token,
-      document_url: `${url}/print`,
-      type: 'pdf',
-      name: title,
-      test: process.env.PDF_TEST || false,
-      prince_options: {
-        baseurl: 'https://beta.u4.no', // URL to use for generating absolute URLs for assets from relative URLs
-        //   media:   "screen",          // use screen styles instead of print styles
+const pdfConfig = ({ url = '', title = 'output' }) => {
+  const token = process.env.DOCRAPTOR_API_KEY;
+  return {
+    url: `https://${token}@docraptor.com/docs`,
+    method: 'POST',
+    encoding: null, // IMPORTANT! This produces a binary body response instead of text
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      doc: {
+        user_credentials: token,
+        document_url: `${url}/print`,
+        type: 'pdf',
+        name: title,
+        test: process.env.PDF_TEST || false,
+        prince_options: {
+          baseurl: 'https://beta.u4.no', // URL to use for generating absolute URLs for assets from relative URLs
+          //   media:   "screen",          // use screen styles instead of print styles
+        },
       },
     },
-  },
-  responseType: 'stream',
-});
+    responseType: 'stream',
+  };
+};
 
 module.exports = { pdfConfig };
