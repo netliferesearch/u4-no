d7e4e7d812ee1dd8c53840f37005247bf1e85d63
Knut Melvær <knut.melvaer@netlife.com>
2017-12-18 07:51:58 +0100

    change translations variable

--- frontend/components/PublicationArticleHeader.js
+++ frontend/components/PublicationArticleHeader.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import { Link } from '../routes';
 import randomKey from '../helpers/randomKey';
@@ -117,3 +118,124 @@ const PublicationArticleHeader = ({
 );
 
 export default PublicationArticleHeader;
+=======
+import React from 'react';
+import { Link } from '../routes';
+import randomKey from '../helpers/randomKey';
+import { Download, ArrowRight } from './icons';
+import { AuthorList, EditorList } from '../components/';
+import BEMHelper from 'react-bem-helper';
+
+const classes = BEMHelper({
+  name: 'article-header',
+  prefix: 'c-',
+});
+
+function languageName(langcode) {
+  const languageNames = {
+    en_US: 'English',
+    fr_FR: 'French',
+    es_ES: 'Spanish',
+    de_DE: 'German',
+    pt_PT: 'Portuguese',
+    ru_RU: 'Russian',
+    uk_UA: 'Ukranian',
+  };
+  return languageNames[langcode] ? languageNames[langcode] : 'another language';
+}
+
+const PublicationArticleHeader = ({
+  title = '',
+  subtitle = '',
+  lead = '',
+  slug = {},
+  topics = [],
+  className = '',
+  publicationType = {},
+  authors = [],
+  editors = [],
+  summary = [],
+  pdfFile = {},
+  legacypdf = {},
+  reference = '',
+  translations = {},
+  language = '',
+}) => (
+  <header {...classes('', null, className)}>
+    {/* Wrap in standard grid width until we know better */}
+    <div {...classes('meta')}>
+      {publicationType.title && `${publicationType.title} | `}
+      {topics.map(({ _ref = '', target = {} }) => (
+        <Link
+          key={_ref}
+          route="topic.entry"
+          params={{ slug: target.slug ? target.slug.current : '' }}
+        >
+          <a {...classes('link-item')}>{target.title}</a>
+        </Link>
+      ))}
+    </div>
+    <div>
+      <h1 {...classes('title')}>{title}</h1>
+    </div>
+    <div>
+      <p {...classes('subtitle')}>{subtitle}</p>
+      <div {...classes('meta')}>
+        <p>
+          {authors.length ? (
+            <span>
+              <AuthorList authors={authors} />
+              <br />
+            </span>
+          ) : null}
+          {editors.length ? (
+            <span>
+              <EditorList editors={editors.map(({ target }) => target)} />
+              <br />
+            </span>
+          ) : null}
+          {reference}
+        </p>
+        {translations.language &&
+          translations.language !== language && (
+            <p>
+              <Link route="publication.entry" params={{ slug: translation.slug.current }}>
+                <a {...classes('language')}>
+                  Also available in {languageName(translation.language)}
+                </a>
+              </Link>
+            </p>
+          )}
+      </div>
+      {summary.length > 0 && (
+        <Link route="publication.shortVersion" params={{ slug: slug.current }}>
+          <a {...classes('button')}>
+            <div {...classes('button-text')}>Read our short version</div>
+            <div {...classes('button-icon')}>
+              <ArrowRight />
+            </div>
+          </a>
+        </Link>
+      )}
+      {pdfFile.asset && (
+        <div {...classes('meta', null, 'c-article-header__download')}>
+          <a href={pdfFile.asset.url} {...classes('download-text')}>
+            <span>Download as PDF</span>
+            <Download {...classes('download-icon')} />
+          </a>
+        </div>
+      )}
+      {legacypdf.asset && (
+        <div {...classes('meta', null, 'c-article-header__download')}>
+          <a href={legacypdf.asset.url} {...classes('download-text')}>
+            <span>Download PDF</span>
+            <Download {...classes('download-icon')} />
+          </a>
+        </div>
+      )}
+    </div>
+  </header>
+);
+
+export default PublicationArticleHeader;
+>>>>>>> change translations variable
