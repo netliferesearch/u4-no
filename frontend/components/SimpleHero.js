22e68da3a5c5143d719b2f6999b7b129d2b6a81e
maris <mariselnes@users.noreply.github.com>
2017-12-18 23:19:40 +0100

    styled helpdesk hero

--- frontend/components/SimpleHero.js
+++ frontend/components/SimpleHero.js
@@ -13,7 +13,7 @@ const classes = BEMHelper({
 const SimpleHero = ({
   title, content = [], light = false, cta = false,
 }) => (
-  <div {...classes(null, light ? 'light' : null)}>
+  <div {...classes(null, light ? 'light' : null, 'o-wrapper-full-width')}>
     <h1 {...classes('title')}>{title}</h1>
     <div {...classes('content')}>
       {isArray(content) && <BlockContent blocks={content} serializers={serializers} />}
