7e7bcd4c7682f43e96e815dd553a4b08cc502b71
Robert Sjursen <robert.sjursen@cmi.no>
2017-12-20 14:09:09 +0100

    Use U4 affiliation for The Team instead of CMI

--- frontend/pages/persons.index.js
+++ frontend/pages/persons.index.js
@@ -23,9 +23,7 @@ class Persons extends Component {
 
   render() {
     console.log(this.props);
-    const {
-      frontpage, persons, helpdesk, affiliatedexperts, url,
-    } = this.props;
+    const { frontpage, persons, helpdesk, affiliatedexperts, url } = this.props;
     return (
       <Layout
         headComponentConfig={{
@@ -34,10 +32,11 @@ class Persons extends Component {
           url: url.asPath ? `beta.u4.no${url.asPath}` : '',
         }}
       >
-
         <BreadCrumb url={url} />
         <div className="u-bg-dark-blue o-wrapper-full-width">
-          <div className="c-article__lead c-article__lead--big-light   c-article__lead--center">{frontpage.lead}</div>
+          <div className="c-article__lead c-article__lead--big-light   c-article__lead--center">
+            {frontpage.lead}
+          </div>
           <div className="c-person__team">
             <h1 className="c-person__team-text ">{frontpage.title}</h1>
             <div className="c-person__team-arrow">
@@ -46,7 +45,7 @@ class Persons extends Component {
           </div>
         </div>
 
-        <div className="c-filters--standalone " >
+        <div className="c-filters--standalone ">
           <div className="c-filters__label--standalone">
             Filter people by:
             <button className="c-filters__item--standalone" onClick={() => this.changeGroup(1)}>
@@ -100,7 +99,7 @@ export default DataLoader(Persons, {
   queryFunc: ({ query: { slug = '' } }) => ({
     sanityQuery: `{
       "frontpage": *[_id == "627b8d42-d8f7-4cf6-9567-f6337678b688"][0],
-      "persons": *[_type == "person" && references("0e1b0ebc-e016-4d2b-97e8-859b91e3e147")][0..100]{..., "image": image.asset->url[0], affiliations},
+      "persons": *[_type == "person" && references("419c2497-8e24-4599-9028-b5023830c87f")][0..100]{..., "image": image.asset->url[0], affiliations},
       "helpdesk": *[_type == "person" && references("17ec3576-0afa-4203-9626-a38a16b27c2a")][0..100]{..., "image": image.asset->url[0], affiliations},
       "affiliatedexperts": *[_type == "person" && references("3babc8f1-9e38-4493-9823-a9352b46585b")][0..100]{..., "image": image.asset->url[0], affiliations},
     }`,
