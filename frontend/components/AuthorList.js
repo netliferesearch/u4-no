b126c7683b6f96a00ce54d19bf3e0562e0c4cd22
Robert Sjursen <robert.sjursen@cmi.no>
2018-01-09 11:27:25 +0100

    Only link to authors who have affiliation with U4

--- frontend/components/AuthorList.js
+++ frontend/components/AuthorList.js
@@ -8,15 +8,22 @@ const AuthorList = ({ authors = false }) => (
       authors
         .filter(author => author)
         .map(author => (author.target ? author.target : author))
-        .map(({ firstName = '', surname = '', slug = false }, index) => (
+        .map(({ firstName = '', surname = '', slug = false, affiliations = [] }, index) => (
           <span key={randomKey()}>
-            {slug ? (
+            {slug &&
+            affiliations.length &&
+            affiliations.some(
+              ({ _ref }) =>
+                _ref === '419c2497-8e24-4599-9028-b5023830c87f' ||
+                _ref === '17ec3576-0afa-4203-9626-a38a16b27c2a' ||
+                _ref === '3babc8f1-9e38-4493-9823-a9352b46585b',
+            ) ? (
               <a href={`/the-team/${slug.current}`}>
-                {firstName} {surname}
-              </a>
-            ) : (
-              `${firstName} ${surname}`
-            )}
+                  {firstName} {surname}
+                </a>
+              ) : (
+                `${firstName} ${surname}`
+              )}
             {/* Add delimiters between the authors. */}
             {authors.length > 1 && index + 2 < authors.length && <span>, </span>}
             {authors.length > 1 && index + 2 === authors.length && <span> and </span>}
