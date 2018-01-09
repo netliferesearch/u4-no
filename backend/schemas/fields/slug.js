7ecd501771d8dad7819111658710d76c2ed2a487
Robert Sjursen <robert.sjursen@cmi.no>
2018-01-09 14:09:31 +0100

    Update slugify function to remove non alphanumeric characters

--- backend/schemas/fields/slug.js
+++ backend/schemas/fields/slug.js
@@ -6,6 +6,10 @@ export default {
     slugify: input => input
                          .toLowerCase()
                          .replace(/\s+/g, '-')
+                         .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                         .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                         .replace(/^-+/, '')             // Trim - from start of text
+                         .replace(/-+$/, '')             // Trim - from end of text
                          .slice(0, 200)
   }
 }
