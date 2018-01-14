fedd928b8a1a738e192a6d2cfd99b2be80aeb30d
Knut Melvær <knut.melvaer@netlife.com>
2018-01-14 23:08:07 +0100

    add weak reference to topics migration

---
+++ data-migration-scripts/update-reference-to-weak.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const sanityClient = require('@sanity/client');
+const client = sanityClient({
+  projectId: '1f1lcoov',
+  dataset: 'production',
+  token: process.env.SANITY_TOKEN,
+  useCdn: true
+});
+
+
+(async () => {
+  const publications = await client.fetch('*[_type == "publication"][0..1000]');
+  /* eslint-disable */
+  for (publication of publications) {
+    if (publication.hasOwnProperty('topics')) {
+      const topics = publication.topics.reduce((acc, curr) => [{...curr, _weak: true}, ...acc], [])
+      console.log(topics)
+      await client
+        .patch(publication._id)
+        .set({topics})
+        .commit()
+        .then(msg => console.log(msg))
+        .catch(err => console.log(err))
+    }
+  }
+})()
