43122918854cb66333338bd187e1296534a303e7
Knut Melvær <knut.melvaer@netlife.com>
2018-01-25 06:35:47 +0100

    add better buttons and download button for assets

--- frontend/pages/asset.entry.js
+++ frontend/pages/asset.entry.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import BEMHelper from 'react-bem-helper';
 
 import { Layout, PdfViewer } from '../components';
+import { Download } from '../components/icons'
 import BreadCrumb from '../components/BreadCrumb';
 import DataLoader from '../helpers/data-loader';
+
+const classes = BEMHelper({
+  name: 'article-header',
+  prefix: 'c-',
+});
 
 const Asset = (props) => {
   const { title = '', slug = {}, asset = {} } = props;
   return (
     <Layout>
+      <header>
+        <div className="u-tc u-margin-bottom">
+          <a href={asset.asset.url} {...classes('download-text')}>
+            <span>Download PDF</span>
+            <Download {...classes('download-icon')} />
+          </a>
+        </div>
+      </header>
       <PdfViewer
         file={{
           url: asset.asset.url,
