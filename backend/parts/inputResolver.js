0a26560afd35ca1a46bc88915645797d120ace1c
Knut Melvær <knut.melvaer@netlife.com>
2018-01-29 12:48:01 +0100

    disable readable integration on caption text

--- backend/parts/inputResolver.js
+++ backend/parts/inputResolver.js
@@ -3,7 +3,8 @@ import FunkyTable from '../components/FunkyTable/FunkyTable'
 import {get} from 'lodash'
 
 export default function resolveInput(type) {
-  if (type.name === 'array' && type.of.find(ofType => ofType.name === 'block')) {
+  console.log(type)
+  if ((type.title === 'caption text'.toLowerCase()) && type.name === 'array' && type.of.find(ofType => ofType.name === 'block')) {
     return FunkyEditor
   }
   if (type.name === 'funkyTable') {
