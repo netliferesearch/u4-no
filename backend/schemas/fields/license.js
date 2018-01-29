b7cf5471632868e6d9350c573c4cc868107e837c
Knut Melvær <knut.melvaer@netlife.com>
2018-01-29 22:37:29 +0100

    update licenses

--- backend/schemas/fields/license.js
+++ backend/schemas/fields/license.js
@@ -1,20 +1,20 @@
 export default {
   name: "license",
   title: "License",
+  description: "See https://creativecommons.org for more information on Creative Commons",
   type: "string",
   options: {
     isHighlighted: true,
     list: [
-      { title: 'Copyright', value: 'copy' },
-      { title: 'CC0', value: 'cc0'},
-      { title: 'Copyright', value: 'copyright'},
-      { title: 'Attribution', value: 'by' },
-      { title: 'Share-alike', value: 'sa' },
-      { title: 'Non-commercial', value: 'nc' },
-      { title: 'No derivative work', value: 'nd' },
-      { title: 'Public Domain', value: 'pd' },
-      { title: 'Public Domain Dedication', value: 'cc0' },
-      { title: 'Creative Commons', value: 'cc' },
+      { title: 'Attribution-NonCommercial-NoDerivs (by-nc-nd)', value: 'by-nc-nd' },
+      { title: 'Attribution-NonCommercial-ShareAlike (by-nc-sa)', value: 'by-nc-sa' },
+      { title: 'Attribution-NonCommercial-ShareAlike (by-nc)', value: 'by-nc' },
+      { title: 'Attribution-NoDerivs (by-nd)', value: 'by-nd' },
+      { title: 'Attribution ShareAlike (by-sa)', value: 'by-sa' },
+      { title: 'Attribution (by)', value: 'by' },
+      { title: 'Public Domain Mark (pd)', value: 'pd' },
+      { title: 'Public Domain Dedication (cc0)', value: 'cc0' },
+      { title: 'Copyrighted (©)', value: 'copyrighted' },
     ], // <-- predefined values
   },
   layout: "dropdown"
