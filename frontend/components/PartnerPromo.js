637028c1e32da376c869ca7b16742307d8e834ef
Robert Sjursen <robert.sjursen@cmi.no>
2018-01-30 10:47:16 +0100

    Add links to PartnerPromo

--- frontend/components/PartnerPromo.js
+++ frontend/components/PartnerPromo.js
@@ -10,43 +10,67 @@ const classes = BEMHelper({
 });
 
 const PartnerPromo = ({
-  title = { __html: '<span>Are you U4 partner staff?<br />Take advantage of exclusive U4 services.</span>' },
+  title = {
+    __html: '<span>Are you U4 partner staff?<br />Take advantage of exclusive U4 services.</span>',
+  },
 }) => (
   <section {...classes('')}>
     <h2 {...classes('title')} dangerouslySetInnerHTML={title} />
 
     <section className="c-boxOnImage c-boxOnImage--smaller">
       <div className="c-boxOnImage__body c-boxOnImage__body--smaller">
-        <div {...classes('statement')}>
-          <OnlineTraining {...classes('statement-icon')} />
-          <div {...classes('statement-bodywrapper')}>
-            <a {...classes('statement-link')} href="/online-courses"><h3 {...classes('statement-title')}>Online training</h3></a>
-            <p {...classes('statement-body')}>Be confident with anti-corruption initiatives in your work. We offer dynamic and time-efficient online courses.</p>
+        <a href="/online-courses">
+          <div {...classes('statement')}>
+            <OnlineTraining {...classes('statement-icon')} />
+            <div {...classes('statement-bodywrapper')}>
+              <a {...classes('statement-link')} href="/online-courses">
+                <h3 {...classes('statement-title')}>Online training</h3>
+              </a>
+              <p {...classes('statement-body')}>
+                Be confident with anti-corruption initiatives in your work. We offer dynamic and
+                time-efficient online courses.
+              </p>
+            </div>
           </div>
-        </div>
-        <div {...classes('statement')}>
-          <Workshops {...classes('statement-icon')} />
-          <div {...classes('statement-bodywrapper')}>
-            <a {...classes('statement-link')} href="/workshops-and-events"><h3 {...classes('statement-title')}>Workshops and events</h3></a>
-            <p {...classes('statement-body')}>Our team can help bring momentum to processes. We gather different actors for informed discussions.</p>
+        </a>
+        <a href="/workshops-and-events">
+          <div {...classes('statement')}>
+            <Workshops {...classes('statement-icon')} />
+            <div {...classes('statement-bodywrapper')}>
+              <a {...classes('statement-link')} href="/workshops-and-events">
+                <h3 {...classes('statement-title')}>Workshops and events</h3>
+              </a>
+              <p {...classes('statement-body')}>
+                Our team can help bring momentum to processes. We gather different actors for
+                informed discussions.
+              </p>
+            </div>
           </div>
-        </div>
-        <div {...classes('statement')}>
-          <Helpdesk {...classes('statement-icon')} />
-          <div {...classes('statement-bodywrapper')}>
-            <a {...classes('statement-link')} href="/helpdesk"><h3 {...classes('statement-title')}>Helpdesk for partners</h3></a>
-            <p {...classes('statement-body')}>Send in your corruption-related questions about development programmes, sectors, or countries. A free research service.</p>
+        </a>
+        <a href="/helpdesk">
+          <div {...classes('statement')}>
+            <Helpdesk {...classes('statement-icon')} />
+            <div {...classes('statement-bodywrapper')}>
+              <a {...classes('statement-link')} href="/helpdesk">
+                <h3 {...classes('statement-title')}>Helpdesk for partners</h3>
+              </a>
+              <p {...classes('statement-body')}>
+                Send in your corruption-related questions about development programmes, sectors, or
+                countries. A free research service.
+              </p>
+            </div>
           </div>
-        </div>
+        </a>
       </div>
       <figure className="c-boxOnImage__figure c-boxOnImage__figure--smaller">
         <img src="/static/services_partners.jpg?w=1600&amp;h=400&amp;fit=crop&amp;crop=focalpoint" />
       </figure>
-
     </section>
     <h2 className="c-topic-section__cta u-margin-top-huge">
       <Link to="/u4-partner-agencies">
-        <a>See all our partners &nbsp;<ArrowRight /></a>
+        <a>
+          See all our partners &nbsp;<ArrowRight />
+        </a>
       </Link>
     </h2>
   </section>
