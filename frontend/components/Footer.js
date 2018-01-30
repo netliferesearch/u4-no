156453918fdfc287721b238ac570a240a88f8c25
Robert Sjursen <robert.sjursen@cmi.no>
2018-01-30 14:50:16 +0100

    Correct footer link to open access policy

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,13 +2,7 @@ import React from 'react';
 import BEMHelper from 'react-bem-helper';
 import { Logo } from './';
 import { Link } from '../routes';
-import {
-  CmiLogo,
-  MediumLogo,
-  TwitterLogo,
-  FacebookLogo,
-  LinkedIn,
- } from './icons/';
+import { CmiLogo, MediumLogo, TwitterLogo, FacebookLogo, LinkedIn } from './icons/';
 
 const classes = BEMHelper({
   name: 'footer',
@@ -33,8 +27,10 @@ const Footer = ({ title }) => (
         </div>
         <div {...classes('item')}>
           <div {...classes('heading')}>Contact</div>
-          <a href="mailto:u4@cmi.no">u4@cmi.no</a><br />
-          <a href="tel:004747938000">+ 47 479 38 000</a><br />
+          <a href="mailto:u4@cmi.no">u4@cmi.no</a>
+          <br />
+          <a href="tel:004747938000">+ 47 479 38 000</a>
+          <br />
         </div>
         <div {...classes('item')}>
           <div {...classes('heading')}>Address</div>
@@ -45,20 +41,34 @@ const Footer = ({ title }) => (
         <div {...classes('item')}>
           <Link to="/privacy-policy">
             <a>Privacy Policy</a>
-          </Link><br />
-          <Link to="/about-u4#openaccess">
+          </Link>
+          <br />
+          <Link to="/about-u4#open-access-policy">
             <a>Open access policy</a>
-          </Link><br />
+          </Link>
+          <br />
           <Link to="/about-u4">
             <a>About U4</a>
-          </Link><br />
+          </Link>
+          <br />
         </div>
         <div {...classes('item')}>
           <div {...classes('heading')}>Follow us</div>
-          <a href="https://www.linkedin.com/showcase/u4-anti-corruption-resource-centre/"><LinkedIn {...classes('some-icon')} />LinkedIn</a><br />
-          <a href="https://medium.com/u4-anti-corruption-resource-centre"><MediumLogo {...classes('some-icon')} />Medium</a><br />
-          <a href="https://twitter.com/U4_ACRC"><TwitterLogo {...classes('some-icon')} />Twitter</a><br />
-          <a href="https://www.facebook.com/U4anticorruption/"><FacebookLogo {...classes('some-icon')} />Facebook</a>
+          <a href="https://www.linkedin.com/showcase/u4-anti-corruption-resource-centre/">
+            <LinkedIn {...classes('some-icon')} />LinkedIn
+          </a>
+          <br />
+          <a href="https://medium.com/u4-anti-corruption-resource-centre">
+            <MediumLogo {...classes('some-icon')} />Medium
+          </a>
+          <br />
+          <a href="https://twitter.com/U4_ACRC">
+            <TwitterLogo {...classes('some-icon')} />Twitter
+          </a>
+          <br />
+          <a href="https://www.facebook.com/U4anticorruption/">
+            <FacebookLogo {...classes('some-icon')} />Facebook
+          </a>
         </div>
       </div>
     </div>
