de005c3b02a2d0d0707cc8f9525409ccf7ef973c
Knut Melvær <knut.melvaer@netlife.com>
2018-01-31 22:14:36 +0100

    move map to support asset references in mosaic

--- frontend/components/Mosaic.js
+++ frontend/components/Mosaic.js
@@ -5,10 +5,10 @@ import { Link } from '../routes';
 const Mosaic = ({ resources = [], alt = false }) => (
   <div className="c-mosaic">
     {resources
+      .map(resource => (resource.target ? resource.target : resource))
       .filter(({ _id = '' }) => _id)
       // loops through lists and return only a set with resources with unique ids
       .reduce((x, y) => (x.map(({ _id }) => _id).includes(y._id) ? x : [...x, y]), [])
-      .map(resource => (resource.target ? resource.target : resource))
       .map((
           {
             title = '',
