397c4d2481b7a20624e578df5b7aa599627174dc
Knut Melvær <knut.melvaer@netlife.com>
2018-02-19 19:08:28 +0100

    only expose token if on a staging environment

--- frontend/env-config.js
+++ frontend/env-config.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: './.env' });
 const prod = process.env.NODE_ENV === 'production';
+const stage = process.env.STAGING === 'true';
 
 module.exports = {
-  'process.env.SANITY_TOKEN': prod ? '' : process.env.SANITY_TOKEN,
+  'process.env.SANITY_TOKEN': stage ? '' : process.env.SANITY_TOKEN,
 };
