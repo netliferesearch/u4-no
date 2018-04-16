43ca7b60d234ec6ae97afba73f82df82e3096b48
Robert Sjursen <robert.sjursen@cmi.no>
2018-04-16 14:23:11 +0200

    Don't show We also recommend if relatedContent empty fix

--- frontend/components/LongformArticleContainer.js
+++ frontend/components/LongformArticleContainer.js
@@ -246,12 +246,13 @@ const LongFormArticleContainer = (props = {}) => {
                 </div>
               </div>
             )}
-          {props.data.relatedContent.length && (
-            <div className="o-wrapper">
-              <h2>We also recommend</h2>
-              <RecommendedResources relatedContent={props.data.relatedContent} />
-            </div>
-          )}
+          {props.data.relatedContent &&
+            props.data.relatedContent.length && (
+              <div className="o-wrapper">
+                <h2>We also recommend</h2>
+                <RecommendedResources relatedContent={props.data.relatedContent} />
+              </div>
+            )}
           <span id="js-bottom" />
           <Footer />
         </article>
