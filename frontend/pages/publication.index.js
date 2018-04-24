ea967ef25df7b1878d7349ea516bd66e6f698d46
Knut Melvær <knut.melvaer@netlife.com>
2018-04-24 10:29:31 +0200

    WIP: move search query to client

--- frontend/pages/publication.index.js
+++ frontend/pages/publication.index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import moment from 'moment';
 import BEMHelper from 'react-bem-helper';
 import { bindActionCreators } from 'redux';
@@ -16,67 +16,83 @@ const classes = BEMHelper({
   prefix: 'c-',
 });
 
-const Publications = ({ data: { publications = [] }, url = {} }) => (
-  <Layout
-    headComponentConfig={{
-      title: 'Publications',
-      url: url.asPath ? `https://www.u4.no${url.asPath}` : '',
-    }}
-  >
-    <div className="o-wrapper-inner">
-      <h1 className="c-topic-page_longTitle u-margin-bottom-huge">Publications</h1>
-      <ul {...classes('content')}>
-        {publications.map(({
-            _id,
-            _type,
-            slug = {},
-            title = '',
-            subtitle = false,
-            authors = false,
-            editors = false,
-            publicationType = false,
-            date = {},
-            reference = '',
-            publicationNumber = false,
-          }) => (
-            <li {...classes('items')} key={_id}>
-              <span {...classes('items-type')}>
-                {publicationType && <span>{publicationType.title}</span>}
-                {!publicationType && <span>{_type}</span>}
-              </span>
-              <span {...classes('items-date')}>
-                {date && moment(date.utc).format('DD.MM.YYYY')}
-              </span>
-              <br />
-              <Link to={buildUrl({ _type, slug })}>
-                <a {...classes('items-title')}>{title}</a>
-              </Link>
-              <br />
-              <span {...classes('items-subtitle')}>{subtitle}</span>
-              {authors.length ? (
-                <div>
-                  <AuthorList authors={authors} />
-                  <br />
-                </div>
-              ) : null}
-              {editors.length ? (
-                <div>
-                  <EditorList editors={editors} />
-                  <br />
-                </div>
-              ) : null}
-              <div>
-                {publicationNumber
-                  ? `${publicationType.title} ${publicationNumber}`
-                  : `${publicationType.title} ${reference}`}
-              </div>
-            </li>
-          ))}
-      </ul>
-    </div>
-    <Footer />
-  </Layout>
-);
+class Publications extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        publications: [],
+      },
+    };
+  }
+
+  render() {
+    const { url = {} } = this.props;
+    const { data: { publications = [] } } = this.state;
+
+    return (
+      <Layout
+       headComponentConfig={{
+         title: 'Publications',
+         url: url.asPath ? `https://www.u4.no${url.asPath}` : '',
+       }}
+     >
+       <div className="o-wrapper-inner">
+         <h1 className="c-topic-page_longTitle u-margin-bottom-huge">Publications</h1>
+         <ul {...classes('content')}>
+           {publications.map(({
+               _id,
+               _type,
+               slug = {},
+               title = '',
+               subtitle = false,
+               authors = false,
+               editors = false,
+               publicationType = false,
+               date = {},
+               reference = '',
+               publicationNumber = false,
+             }) => (
+               <li {...classes('items')} key={_id}>
+                 <span {...classes('items-type')}>
+                   {publicationType && <span>{publicationType.title}</span>}
+                   {!publicationType && <span>{_type}</span>}
+                 </span>
+                 <span {...classes('items-date')}>
+                   {date && moment(date.utc).format('DD.MM.YYYY')}
+                 </span>
+                 <br />
+                 <Link to={buildUrl({ _type, slug })}>
+                   <a {...classes('items-title')}>{title}</a>
+                 </Link>
+                 <br />
+                 <span {...classes('items-subtitle')}>{subtitle}</span>
+                 {authors.length ? (
+                   <div>
+                     <AuthorList authors={authors} />
+                     <br />
+                   </div>
+                 ) : null}
+                 {editors.length ? (
+                   <div>
+                     <EditorList editors={editors} />
+                     <br />
+                   </div>
+                 ) : null}
+                 <div>
+                   {publicationNumber
+                     ? `${publicationType.title} ${publicationNumber}`
+                     : `${publicationType.title} ${reference}`}
+                 </div>
+               </li>
+             ))}
+         </ul>
+       </div>
+       <Footer />
+     </Layout>
+    );
+  }
+}
 
 export default DataLoader(Publications, {
   queryFunc: () => ({
