e68f9461586956dac3f3983b44c3b3c928b8d54e
Nils Norman Haukås <mail@nilsnh.no>
2019-01-04 14:50:56 +0100

    register pageview when clicking to download pdf

--- frontend/components/HeadComponent.js
+++ frontend/components/HeadComponent.js
@@ -28,17 +28,15 @@ const HeadComponent = ({
     <link rel="icon" type="image/png" href="/static/favicon.png" />
 
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-    {!process.env.DEV && (
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KVW5J96');`,
-        }}
-      />
-    )}
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KVW5J96');`,
+      }}
+    />
   </Head>
 );
 
