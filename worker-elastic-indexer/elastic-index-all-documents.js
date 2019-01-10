e8ddfdedd0c1557b135fa673e755b29236874ed5
Nils Norman Haukås <mail@nilsnh.no>
2019-01-10 15:48:00 +0100

    add setup for bulk inserting publications

--- worker-elastic-indexer/elastic-index-all-documents.js
+++ worker-elastic-indexer/elastic-index-all-documents.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const elasticsearch = require('elasticsearch');
+const _ = require('lodash');
+const { loadSanityDataFile, processPublication } = require('./elastic-indexer.lib');
 
 const client = new elasticsearch.Client({
   host: process.env.ES_HOST,
+  apiVersion: '6.5',
 });
 
 // get Sanity documents
@@ -11,8 +14,42 @@ const client = new elasticsearch.Client({
 
 // new publications: need to collapse down their main content
 
-const prepareElasticSearchBulkInsert = () => {};
+// prepare the call with which Elasticsearch will be called.
+const prepareElasticSearchBulkInsert = (documents = []) => {
+  const _index = 'u4-test';
+  return _.flatten(documents.map((doc) => {
+    const { _type, _id, ...restOfDoc } = doc;
+    const metadata = { _index, _type, _id };
+    return [{ index: metadata }, { ...restOfDoc }];
+  }));
+};
 
-function main() {}
+const insertElasticSearchData = (documents = []) =>
+  new Promise((resolve, reject) => {
+    client.bulk(
+      {
+        body: prepareElasticSearchBulkInsert(documents),
+      },
+      (err, response) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(response);
+      },
+    );
+  });
+
+async function main() {
+  const allDocuments = loadSanityDataFile('./test-data.ndjson');
+  const publications = allDocuments
+    .filter(({ _type }) => _type === 'publication')
+    .map(document => processPublication({ document, allDocuments }));
+  try {
+    const result = await insertElasticSearchData(publications);
+    console.log('Done with work! Api responded with', JSON.stringify(result, null, 2));
+  } catch (e) {
+    console.error('Failed to insert documents', e);
+  }
+}
 
 main();
