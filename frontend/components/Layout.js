bcedc333f506d4ad0ed15019c21b7d388853769e
maris <mariselnes@users.noreply.github.com>
2019-01-18 11:02:24 +0100

    convert Layout to a Class

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import autobind from 'react-autobind';
 import BEMHelper from 'react-bem-helper';
 import { Link } from '../routes';
 import { HeadComponent, Logo, Menu, SearchFieldV2 } from '../components';
@@ -9,41 +10,55 @@ const classes = BEMHelper({
   prefix: 'c-',
 });
 
-const Layout = ({
-  showLoadingScreen = false,
-  showTopTab = true,
-  children = [],
-  noSearch = false,
-  searchV2 = false,
-  headComponentConfig = {},
-  hideLogo = false,
-}) => (
-  <div
-    className="u-print-width o-wrapper-page"
-    style={{
-      transition: 'all 0.1s ease-out',
-      opacity: showLoadingScreen ? 0 : 1,
-    }}
-  >
-    <HeadComponent {...headComponentConfig} />
-    {showTopTab && (
-      <div {...classes('', '', 'u-z-index-xx u-bg-white')}>
-        {!hideLogo && (
+class Layout extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeSearchMenu: false,
+    };
+    autobind(this);
+  }
+
+  render() {
+    const {
+      showLoadingScreen = false,
+      showTopTab = true,
+      children = [],
+      noSearch = false,
+      searchV2 = false,
+      headComponentConfig = {},
+      hideLogo = false,
+    } = this.props;
+
+    return (
+      <div
+        className="u-print-width o-wrapper-page"
+        style={{
+          transition: 'all 0.1s ease-out',
+          opacity: showLoadingScreen ? 0 : 1,
+        }}
+      >
+        <HeadComponent {...headComponentConfig} />
+        {showTopTab && (
+        <div {...classes('', '', 'u-z-index-xx u-bg-white')}>
+          {!hideLogo && (
           <Link route="/">
             <a {...classes('logo')}>
               <Logo />
             </a>
           </Link>
-        )}
+              )}
 
-
-        {hideLogo && <div />}
-        <Menu noSearch={noSearch} />
+          {hideLogo && <div />}
+          <Menu noSearch={noSearch} />
+        </div>
+          )}
+        {children}
       </div>
-    )}
-    {children}
-  </div>
-);
+    );
+  }
+}
 
 Layout.propTypes = {
   showLoadingScreen: PropTypes.bool,
