bd1e420a8bd78f2e79a89c5a7a9b8df836477bae
maris <mariselnes@users.noreply.github.com>
2019-01-21 11:22:52 +0100

    Make search page the same width as header

--- frontend/pages/search-v2.index.js
+++ frontend/pages/search-v2.index.js
@@ -35,7 +35,7 @@ const Search = ({
         url: url.asPath ? `https://www.u4.no${url.asPath}` : '',
       }}
     >
-      <div className="o-wrapper o-wrapper--padded o-layout o-layout--flush">
+      <div className="o-wrapper o-wrapper--padded-large o-layout o-layout--flush">
         <section className=" o-layout__item u-12/12 u-3/12@desktop">
           <SearchFiltersV2 />
         </section>
