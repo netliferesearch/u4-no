7273c96333f7c9dbff754c5a573ff4bb27768e11
maris <mariselnes@users.noreply.github.com>
2019-01-24 14:48:58 +0100

    Show search results based on sorting

--- frontend/helpers/elastic-data-loader.js
+++ frontend/helpers/elastic-data-loader.js
@@ -80,11 +80,23 @@ const doSearch = async (query) => {
           },
         },
 
+
         // example of spread syntax
         // docs: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#Spread_in_object_literals
         //
         // Elastic: https://www.elastic.co/guide/en/elasticsearch/reference/current/search-request-sort.html
         ...(false ? { hello: 'world' } : {}),
+
+        ...(sort === 'year-desc' ? {
+          sort: [
+            { 'date.utc': { order: 'desc' } },
+          ],
+        } : sort === 'year-asc' ? {
+          sort: [
+            { 'date.utc': { order: 'asc' } },
+          ],
+        } : {}),
+
 
         highlight: {
           fields: {
