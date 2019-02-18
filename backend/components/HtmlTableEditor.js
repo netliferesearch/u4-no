bf6ccb71219f0eaf7334ae8867cd86af9b1f0c19
Nils Norman Haukås <mail@nilsnh.no>
2019-02-18 11:33:19 +0100

    TinyMCE: Deactivate paid enterprise plugins

--- backend/components/HtmlTableEditor.js
+++ backend/components/HtmlTableEditor.js
@@ -43,7 +43,7 @@ export default class WrappedDefaultInput extends React.Component {
       <Editor
       id="tinymce-editor"
       apiKey="ivxiggexqgnqeg3xjjiczxw06ed4btfaszkj8g8u2u64ifkt" init={{
-        plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+        plugins: 'print preview fullpage searchreplace autolink directionality visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern help',
         toolbar: 'table | formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
         height: '800'
       }}
