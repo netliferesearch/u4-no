c260dfa5d8fd4e0e46d964068659f55c7cc63258
Nils Norman Haukås <mail@nilsnh.no>
2019-05-08 15:08:16 +0200

    delete helpers that were not imported anywhere

--- frontend/helpers/client.js
+++
@@ -1,8 +0,0 @@
-import sanityClient from '@sanity/client';
-
-export default sanityClient({
-  projectId: '1f1lcoov',
-  dataset: 'production',
-  token: process.env.SANITY_TOKEN,
-  useCdn: true,
-});
