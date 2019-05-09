49753d794144206098cd40fceffe054dae7a5cb5
Nils Norman Haukås <mail@nilsnh.no>
2019-05-09 13:57:06 +0200

    css fix for menu

--- frontend/components/MenuV2.js
+++ frontend/components/MenuV2.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import BEMHelper from 'react-bem-helper';
 import sanityClient from '@sanity/client';
 import PropTypes from 'prop-types';
@@ -74,7 +74,7 @@ class MenuV2 extends Component {
     const topics = this.state.data;
     const { noSearch, triggerSearchMenu, activeSearchMenu } = this.props;
     return (
-      <Fragment>
+      <div>
         <ul {...classes('menu')}>
           {!noSearch && !activeSearchMenu && (
             <li {...classes('menu-item')}>
@@ -214,7 +214,7 @@ class MenuV2 extends Component {
             </div>
           </div>
         ) : null}
-      </Fragment>
+      </div>
     );
   }
 }
