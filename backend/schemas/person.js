455e833b8f395a9d0101eedf2ce88919191c30c9
Robert Sjursen <robert.sjursen@cmi.no>
2019-09-23 15:40:19 +0200

    Add field: Short Bio French version

--- backend/schemas/person.js
+++ backend/schemas/person.js
@@ -118,6 +118,35 @@ export default {
       ],
     },
     {
+      name: 'bioShort_fr',
+      title: 'Short biography (French)',
+      description: 'For publications on pdfs etc. Typically just one paragraph.',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            {title: 'Normal', value: 'normal'},
+            {title: 'H2', value: 'h2'},
+            {title: 'H3', value: 'h3'},
+          ],
+          // Only allow numbered lists
+          marks: {
+            // Only allow these decorators
+            decorators: [
+              {title: 'Emphasis', value: 'em'}
+            ],
+            // Support annotating text with a reference to an author
+            annotations: [
+              {name: 'link', title: 'External Link', type: 'object', fields: [{ name: 'href', title: 'URL', type: 'url'}] },
+              {name: 'internalReferance', title: 'Author or publication', type: 'reference', to: [{type: 'person'},{type: 'publication'},{type: 'article'}]},
+            ]
+          }
+        },
+      ],
+    },
+
+    {
       name: 'image',
       type: 'image',
       options: {
