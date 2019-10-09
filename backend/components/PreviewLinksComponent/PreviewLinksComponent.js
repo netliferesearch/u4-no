9cd840137d46915d07eb4ba1e19a33cb8cb6132c
Robert Sjursen <robert.sjursen@cmi.no>
2019-10-09 15:49:21 +0200

    Show preview links only if slug available

--- backend/components/PreviewLinksComponent/PreviewLinksComponent.js
+++ backend/components/PreviewLinksComponent/PreviewLinksComponent.js
@@ -12,55 +12,61 @@ class PreviewLinksComponent extends React.PureComponent {
   }
   render() {
     return (
-      <div className={styles.buttonGroup}>
-        {this.props.document._id.startsWith('draft') && (
-          <span>
-            <AnchorButton
-              inverted="true"
-              padding="small"
-              href={`${previewUrl}/preview/${this.props.document._type}/${this.props.document._id}`}
-              target="_blank"
-              title="Preview draft page"
-            >
-              Preview
-            </AnchorButton>
-            {this.props.document._type === 'publication' && (
-
+      !this.props.document.slug ? null : (
+        <div className={styles.buttonGroup}>
+          {this.props.document._id.startsWith('draft') && (
+            <span className={styles.buttonSubgroup}>
               <AnchorButton
-                inverted="true"
+                inverted={true}
                 padding="small"
-                href={`${previewUrl}/previewpdf/${this.props.document._type}/${
+                href={`${previewUrl}/preview/${this.props.document._type}/${
                   this.props.document._id
                 }`}
                 target="_blank"
-                title="Preview draft as pdf"
+                title="Preview draft page"
               >
-                Preview pdf
+                Preview
+              </AnchorButton>
+              {this.props.document._type === 'publication' && (
+                <AnchorButton
+                  inverted={true}
+                  padding="small"
+                  href={`${previewUrl}/previewpdf/${this.props.document._type}/${
+                    this.props.document._id
+                  }`}
+                  target="_blank"
+                  title="Preview draft as pdf"
+                >
+                  Preview pdf
+                </AnchorButton>
+              )}
+            </span>
+          )}
+          <span className={styles.buttonSubgroup}>
+            <AnchorButton
+              inverted={true}
+              padding="small"
+              href={`${previewUrl +
+                buildUrl({ _type: this.props.document._type, slug: this.props.document.slug })}`}
+              target="_blank"
+              title="View page on site"
+            >
+              View
+            </AnchorButton>
+            {this.props.document._type === 'publication' && (
+              <AnchorButton
+                inverted={true}
+                padding="small"
+                href={`${previewUrl}/publications/${this.props.document.slug.current}.pdf`}
+                target="_blank"
+                title="View pdf on site"
+              >
+                View pdf
               </AnchorButton>
             )}
           </span>
-        )}
-        <AnchorButton
-          inverted="true"
-          padding="small"
-          href={`${previewUrl + buildUrl({_type: this.props.document._type, slug: this.props.document.slug })}`}
-          target="_blank"
-          title="View page on site"
-        >
-          View
-        </AnchorButton>
-        {this.props.document._type === 'publication' && (
-
-        <AnchorButton
-          inverted="true"
-          padding="small"
-          href={`${previewUrl}/publications/${this.props.document.slug.current}.pdf`}
-          target="_blank"
-          title="View pdf on site"
-        >
-          View pdf
-        </AnchorButton>)}
-      </div>
+        </div>
+      )
     );
   }
 }
