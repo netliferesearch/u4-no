69300fed3f611697468461a81d61fff037037b63
Mantas_Venskus <mantas.venskus1@gmail.com>
2020-01-03 11:20:25 +0200

    Frontpage v2 and associated components added

---
+++ frontend/components/v2/index.js
@@ -0,0 +1,10 @@
+export { default as Menu } from './Menu';
+export { default as Layout } from './Layout';
+export { default as CorruptionByTopic } from './CorruptionByTopic';
+export { default as FeaturedPosts } from './FeaturedPosts';
+export { default as Guidance } from './Guidance';
+export { default as InsightPosts } from './InsightPosts';
+export { default as NewsAndEvents } from './NewsAndEvents';
+export { default as RecentPublications } from './RecentPublications';
+export { default as LogoCMI } from './LogoCMI';
+export { default as SearchField } from './SearchField';
