cbea538afd596715877f4f85439f694ac8fb108e
Mantas_Venskus <mantas.venskus1@gmail.com>
2020-01-10 10:43:54 +0200

    sanity client set to always use production dataset

--- frontend/helpers/sanity-client-config.js
+++ frontend/helpers/sanity-client-config.js
@@ -1,16 +1,16 @@
 import sanityClient from '@sanity/client';
-const dataset = process.env.REACT_APP_DATASET === 'staging' ? 'staging' : 'production';
+//const dataset = process.env.REACT_APP_DATASET === 'staging' ? 'staging' : 'production';
 
 export const clientWithToken = sanityClient({
   projectId: '1f1lcoov',
-  dataset: dataset,
+  dataset: 'production',
   token: process.env.SANITY_TOKEN,
   useCdn: true,
 });
 
 const client = sanityClient({
   projectId: '1f1lcoov',
-  dataset: dataset,
+  dataset: 'production',
   token: '',
   useCdn: true,
 });
