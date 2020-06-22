735ba1b8e001f867fa12c4bbf5b30ac67cd757bd
Robert Sjursen <robert.sjursen@cmi.no>
2020-06-22 11:27:46 +0200

    Reorder fields in publication schema

--- backend/schemas/publication.js
+++ backend/schemas/publication.js
@@ -16,18 +16,95 @@ export default {
     title,
     subtitle,
     publicationContent,
-    standfirst,
     leadText,
+    standfirst,
     {
-      name: 'pdfFile',
-      title: 'Pdf file (generated)',
-      type: 'file',
+      name: 'mainPoints',
+      title: 'Main points',
+      description:
+        'List 2–10 implications/recommendations/must-knows for development professionals. 1-2 sentences per point.',
+      type: 'array',
+      of: [
+        {
+          type: 'string',
+        },
+      ],
     },
     {
-      name: 'legacypdf',
-      title: 'Pdf file (uploaded)',
-      description: 'This file overrides the generated pdf',
-      type: 'file',
+      name: 'authors',
+      description: 'Place in order of appearance',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'person',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'editors',
+      title: 'Series editors',
+      description: 'Responsible U4 staff member',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'person',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'date',
+      description: 'Date of publication',
+      type: 'richDate',
+      options: {
+        inputUtc: true,
+        dateFormat: 'YYYY-MM-DD',
+        inputDate: true,
+        inputTime: false,
+      },
+    },
+    {
+      name: 'publicationType',
+      title: 'Publication type',
+      type: 'reference',
+      to: [
+        {
+          type: 'publicationType',
+        },
+      ],
+    },
+    {
+      name: 'publicationNumber',
+      title: 'Publication Number',
+      description: 'e.g. "2016:1" ',
+      type: 'string',
+    },
+    {
+      name: 'references',
+      title: 'Publication references',
+      description: 'A list of the sources used in this publication',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          lists: [],
+          styles: [],
+          marks: {
+            // Only allow these decorators
+            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
+            annotations,
+          },
+        },
+      ],
     },
     {
       name: 'featuredImage',
@@ -93,102 +170,36 @@ export default {
       ],
     },
     {
-      name: 'summary',
-      title: 'Short version',
-      description:
-        'One-pager, blog-like, light narrative. Bite-sized chunks with descriptive sub-headings. Explain: 1. the main recommendations and/or implications and findings, and  2. why this is important. Max 1000 words.',
+      name: 'topics',
+      description: 'Select relevant U4 topics',
       type: 'array',
       of: [
         {
-          type: 'block',
-          styles: [
-            { title: 'Normal', value: 'normal' },
-            { title: 'H2', value: 'h2' },
-            { title: 'H3', value: 'h3' },
-            { title: 'H4', value: 'h4' },
-            { title: 'H5', value: 'h5' },
+          type: 'reference',
+          weak: true,
+          to: [
+            {
+              type: 'topics',
+            },
           ],
-          // Only allow numbered lists
-          marks: {
-            // Only allow these decorators
-            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
-            // Support annotating text with a reference to an author
-            annotations,
-          },
         },
-        image,
       ],
-    },
-    {
-      name: 'summaryExternal',
-      title: 'Link to external short version',
-      description: 'Most probably a medium link',
-      type: 'url',
-    },
-    {
-      name: 'date',
-      description: 'Date of publication',
-      type: 'richDate',
-      options: {
-        inputUtc: true,
-        dateFormat: 'YYYY-MM-DD',
-        inputDate: true,
-        inputTime: false,
+      preview: {
+        title: 'topics.title',
       },
     },
     {
-      name: 'methodology',
-      title: 'Methodology',
-      description:
-        '(Optional and only for long texts): A clear statement of the research question and methodology explained.',
+      name: 'keywords',
+      description: 'Chose from drop-down menu of the available U4 keywords.',
       type: 'array',
       of: [
         {
-          type: 'block',
-          styles: [
-            { title: 'Normal', value: 'normal' },
-            { title: 'H2', value: 'h2' },
-            { title: 'H3', value: 'h3' },
-            { title: 'H4', value: 'h4' },
-            { title: 'H5', value: 'h5' },
+          type: 'reference',
+          to: [
+            {
+              type: 'keyword',
+            },
           ],
-          // Only allow numbered lists
-          marks: {
-            // Only allow these decorators
-            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
-            // Support annotating text with a reference to an author
-            annotations,
-          },
-        },
-      ],
-    },
-    {
-      name: 'references',
-      title: 'Publication references',
-      description: 'A list of the sources used in this publication',
-      type: 'array',
-      of: [
-        {
-          type: 'block',
-          lists: [],
-          styles: [],
-          marks: {
-            // Only allow these decorators
-            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
-            annotations,
-          },
-        },
-      ],
-    },
-    {
-      name: 'mainPoints',
-      title: 'Main points',
-      description:
-        'List 2–10 implications/recommendations/must-knows for development professionals. 1-2 sentences per point.',
-      type: 'array',
-      of: [
-        {
-          type: 'string',
         },
       ],
     },
@@ -211,18 +222,58 @@ export default {
         },
       ],
     },
+    slug,
+    shortSlug,
     {
-      name: 'authors',
-      description: 'Place in order of appearance',
+      name: 'pdfFile',
+      title: 'Pdf file (generated)',
+      type: 'file',
+    },
+    {
+      name: 'legacypdf',
+      title: 'Pdf file (uploaded)',
+      description: 'This file overrides the generated pdf',
+      type: 'file',
+    },
+    language,
+    {
+      name: 'translation',
+      title: 'Translation of',
+      description: 'If this publication is the translation of another U4 publication',
+      type: 'reference',
+      to: [
+        {
+          type: 'publication',
+        },
+      ],
+    },
+
+    {
+      name: 'updatedVersion',
+      title: 'Updated version',
+      description: 'If there is a more recent version of this publication',
+      type: 'reference',
+      to: [
+        {
+          type: 'publication',
+        },
+      ],
+    },
+    {
+      name: 'headsUp',
+      title: 'Heads up',
+      description:
+        'Important message to the reader, like "The content in this publication might be outdated"',
       type: 'array',
       of: [
         {
-          type: 'reference',
-          to: [
-            {
-              type: 'person',
-            },
-          ],
+          type: 'block',
+          lists: [],
+          styles: [],
+          marks: {
+            // Only allow these decorators
+            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
+          },
         },
       ],
     },
@@ -243,94 +294,8 @@ export default {
       ],
     },
     {
-      name: 'editors',
-      title: 'Series editors',
-      description: 'Responsible U4 staff member',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [
-            {
-              type: 'person',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      name: 'partners',
-      title: 'Partnership',
-      type: 'array',
-      of: [
-        {
-          name: 'partner',
-          type: 'object',
-          fields: [
-            {
-              type: 'text',
-              name: 'description',
-              description: 'E.g. “This is a joint partnership”',
-            },
-            {
-              type: 'reference',
-              name: 'institution',
-              title: 'institution',
-              to: [
-                {
-                  type: 'institution',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      name: 'acknowledgements',
-      type: 'text',
-    },
-    {
       name: 'abstract',
       type: 'text',
-    },
-    {
-      name: 'keywords',
-      description: 'Chose from drop-down menu of the available U4 keywords.',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          to: [
-            {
-              type: 'keyword',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      name: 'publicationType',
-      title: 'Publication type',
-      type: 'reference',
-      to: [
-        {
-          type: 'publicationType',
-        },
-      ],
-    },
-    {
-      name: 'publicationNumber',
-      title: 'Publication Number',
-      description: 'e.g. "2016:1" ',
-      type: 'string',
-    },
-    {
-      name: 'reference',
-      title: 'Bibliographic reference override',
-      description:
-        'Leave this empty to generate from fields above, like "U4 Anti-Corruption Resource Centre, Chr. Michelsen Institute (U4 Brief 2017:1)""',
-      type: 'string',
     },
     {
       name: 'abbreviations',
@@ -369,71 +334,109 @@ export default {
       },
     },
     {
+      name: 'methodology',
+      title: 'Methodology',
+      description:
+        '(Optional and only for long texts): A clear statement of the research question and methodology explained.',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'H2', value: 'h2' },
+            { title: 'H3', value: 'h3' },
+            { title: 'H4', value: 'h4' },
+            { title: 'H5', value: 'h5' },
+          ],
+          // Only allow numbered lists
+          marks: {
+            // Only allow these decorators
+            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
+            // Support annotating text with a reference to an author
+            annotations,
+          },
+        },
+      ],
+    },
+    {
+      name: 'acknowledgements',
+      type: 'text',
+    },
+    {
+      name: 'summary',
+      title: 'Short version',
+      description:
+        'One-pager, blog-like, light narrative. Bite-sized chunks with descriptive sub-headings. Explain: 1. the main recommendations and/or implications and findings, and  2. why this is important. Max 1000 words.',
+      type: 'array',
+      of: [
+        {
+          type: 'block',
+          styles: [
+            { title: 'Normal', value: 'normal' },
+            { title: 'H2', value: 'h2' },
+            { title: 'H3', value: 'h3' },
+            { title: 'H4', value: 'h4' },
+            { title: 'H5', value: 'h5' },
+          ],
+          // Only allow numbered lists
+          marks: {
+            // Only allow these decorators
+            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
+            // Support annotating text with a reference to an author
+            annotations,
+          },
+        },
+        image,
+      ],
+    },
+    {
+      name: 'summaryExternal',
+      title: 'Link to external short version',
+      description: 'Most probably a medium link',
+      type: 'url',
+    },
+    {
+      name: 'reference',
+      title: 'Bibliographic reference override',
+      description:
+        'Leave this empty to generate from fields above, like "U4 Anti-Corruption Resource Centre, Chr. Michelsen Institute (U4 Brief 2017:1)""',
+      type: 'string',
+    },
+    {
+      name: 'partners',
+      title: 'Partnership',
+      type: 'array',
+      of: [
+        {
+          name: 'partner',
+          type: 'object',
+          fields: [
+            {
+              type: 'text',
+              name: 'description',
+              description: 'E.g. “This is a joint partnership”',
+            },
+            {
+              type: 'reference',
+              name: 'institution',
+              title: 'institution',
+              to: [
+                {
+                  type: 'institution',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
       name: 'blurbs',
       type: 'array',
       of: [
         {
           type: 'blurb',
-        },
-      ],
-    },
-    {
-      name: 'topics',
-      description: 'Select relevant U4 topics',
-      type: 'array',
-      of: [
-        {
-          type: 'reference',
-          weak: true,
-          to: [
-            {
-              type: 'topics',
-            },
-          ],
-        },
-      ],
-      preview: {
-        title: 'topics.title',
-      },
-    },
-    language,
-    {
-      name: 'translation',
-      title: 'Translation of',
-      description: 'If this publication is the translation of another U4 publication',
-      type: 'reference',
-      to: [
-        {
-          type: 'publication',
-        },
-      ],
-    },
-    {
-      name: 'updatedVersion',
-      title: 'Updated version',
-      description: 'If there is a more recent version of this publication',
-      type: 'reference',
-      to: [
-        {
-          type: 'publication',
-        },
-      ],
-    },
-    {
-      name: 'headsUp',
-      title: 'Heads up',
-      description:
-        'Important message to the reader, like "The content in this publication might be outdated"',
-      type: 'array',
-      of: [
-        {
-          type: 'block',
-          lists: [],
-          styles: [],
-          marks: {
-            // Only allow these decorators
-            decorators: [{ title: 'Strong', value: 'strong' }, { title: 'Emphasis', value: 'em' }],
-          },
         },
       ],
     },
@@ -480,9 +483,11 @@ export default {
       type: 'urlWithMetadata',
       inputComponent: UrlWithMetadataInput,
     },
-    slug,
-    shortSlug,
+
   ],
+  initialValue: {
+    language: 'en_US',
+  },
   orderings: [
     {
       title: 'Title',
