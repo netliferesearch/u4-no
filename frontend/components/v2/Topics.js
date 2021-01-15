ff44b6790fdd254151bbc69345c3668bc97badd3
Dagne <dagne@asteroid.lt>
2021-01-15 17:09:17 +0200

    topics slug, reader translations

--- frontend/components/v2/Topics.js
+++ frontend/components/v2/Topics.js
@@ -16,16 +16,18 @@ export const Topics = ({ title = true, topics = [], hr = false }) => {
       {title ? <h4 className="u-headline--3">Topics</h4> : null}
       {hr ? <hr className="u-section-underline--no-margins" /> : null}
       <div className="c-topics__list">
-        {topics.map((topic, index) => (
-          <span className="topic" key={index}>
-            <Link route="topic.entry" params={{ slug: topic.slug.current }}>
-              <a className="c-btn--ter">
-                <div>{topic.title}</div>
-              </a>
-            </Link>
-            <span>{`${topics.length > 1 && index + 1 < topics.length ? ', ' : ''}`}</span>
-          </span>
-        ))}
+        {topics.map((topic, index) =>
+          topic.slug && topic.title ? (
+            <span className="topic" key={index}>
+              <Link route="topic.entry" params={{ slug: topic.slug.current }}>
+                <a className="c-btn--ter">
+                  <div>{topic.title}</div>
+                </a>
+              </Link>
+              <span>{`${topics.length > 1 && index + 1 < topics.length ? ', ' : ''}`}</span>
+            </span>
+          ) : null
+        )}
       </div>
     </div>
   ) : null;
