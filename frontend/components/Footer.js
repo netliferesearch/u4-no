348adc30a3dbb34f60c0d5906144cedfce5ec576
Dagne <dagne@asteroid.lt>
2021-03-01 15:31:38 +0200

    spacing, ajustments

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -15,7 +15,7 @@ const classes = BEMHelper({
 const Footer = () => (
   <footer className="c-footer o-wrapper-medium">
     <div className="c-footer__row u-flex-sb">
-      <a href="https://www.cmi.no/" {...classes('link')}>
+      <a href="https://www.cmi.no/" className="c-footer__logo u-border--none">
         <CmiLogo {...classes('color-icon')} />
       </a>
     </div>
@@ -30,7 +30,7 @@ const Footer = () => (
         <Newsletter cta="Dont miss our next report" text1="" text2="" button="Keep me updated" />
       </div>
     </div>
-    <div className="c-footer__row u-flex-sb">
+    <div className="u-flex-sb">
       <div className="c-footer__col1">
         <div className="u-text--15 u-text--grey">
           <h4 className="u-heading--5">Address</h4>
@@ -41,69 +41,67 @@ const Footer = () => (
         </div>
       </div>
 
-      <div className="c-footer__col2">
-        <div className="c-footer__row u-flex-sb">
-          <div className="c-footer__col c-footer__links--no-h">
-            <Link route="/topics">
-              <a className="c-btn c-btn--qua">
-                <span>Research topics</span>
-              </a>
-            </Link>
+      <div className="c-footer__col2 u-flex-sb">
+        <div className="c-footer__col c-footer__links--no-h">
+          <Link route="/topics">
+            <a className="c-btn c-btn--qua">
+              <span>Research topics</span>
+            </a>
+          </Link>
 
-            <Link route="/blog">
-              <a className="c-btn c-btn--qua">
-                <span>Blog</span>
-              </a>
-            </Link>
+          <Link route="/blog">
+            <a className="c-btn c-btn--qua">
+              <span>Blog</span>
+            </a>
+          </Link>
 
-            <Link route="/courses">
-              <a className="c-btn c-btn--qua">
-                <span>Online course</span>
-              </a>
-            </Link>
-          </div>
-          <div className="c-footer__col c-footer__links">
-            <h4 className="u-heading--5">About U4</h4>
-            <Link route="/the-team">
-              <a className="c-btn c-btn--qua">
-                <span>Team</span>
-              </a>
-            </Link>
+          <Link route="/courses">
+            <a className="c-btn c-btn--qua">
+              <span>Online course</span>
+            </a>
+          </Link>
+        </div>
+        <div className="c-footer__col c-footer__links">
+          <h4 className="u-heading--5">About U4</h4>
+          <Link route="/the-team">
+            <a className="c-btn c-btn--qua">
+              <span>Team</span>
+            </a>
+          </Link>
 
-            <Link route="/about-u4">
-              <a className="c-btn c-btn--qua">
-                <span>Vision & Strategy</span>
-              </a>
-            </Link>
+          <Link route="/about-u4">
+            <a className="c-btn c-btn--qua">
+              <span>Vision & Strategy</span>
+            </a>
+          </Link>
 
-            <Link route="/privacy-policy">
-              <a className="c-btn  c-btn--qua">
-                <span>Privacy policy</span>
-              </a>
-            </Link>
-          </div>
-          <div className="c-footer__col c-footer__links">
-            <h4 className="u-heading--5">Contact</h4>
-            <a className="c-btn c-btn--qua" href="mailto:u4@cmi.no">
-              u4@cmi.no
+          <Link route="/privacy-policy">
+            <a className="c-btn  c-btn--qua">
+              <span>Privacy policy</span>
+            </a>
+          </Link>
+        </div>
+        <div className="c-footer__col c-footer__links">
+          <h4 className="u-heading--5">Contact</h4>
+          <a className="c-btn c-btn--qua" href="mailto:u4@cmi.no">
+            u4@cmi.no
+          </a>
+
+          <a className="c-btn c-btn--qua" href="tel:004747938000">
+            + 47 479 38 000
+          </a>
+
+          <div className="c-share-buttons">
+            <a href="https://www.facebook.com/U4anticorruption/">
+              <FacebookIcon />
+            </a>
+            <a href="https://www.linkedin.com/showcase/u4-anti-corruption-resource-centre/">
+              <LinkedInIcon />
             </a>
 
-            <a className="c-btn c-btn--qua" href="tel:004747938000">
-              + 47 479 38 000
+            <a href="https://twitter.com/U4_ACRC">
+              <TwitterIcon />
             </a>
-
-            <div className="c-share-buttons">
-              <a href="https://www.facebook.com/U4anticorruption/">
-                <FacebookIcon />
-              </a>
-              <a href="https://www.linkedin.com/showcase/u4-anti-corruption-resource-centre/">
-                <LinkedInIcon />
-              </a>
-
-              <a href="https://twitter.com/U4_ACRC">
-                <TwitterIcon />
-              </a>
-            </div>
           </div>
         </div>
       </div>
