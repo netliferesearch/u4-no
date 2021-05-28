d7728b1f5a5e2dd0d56bcf6661458266415df16c
Baynrich <bendikasolevaag@gmail.com>
2021-05-28 11:08:59 +0200

    Add course entry page

--- frontend/routes.js
+++ frontend/routes.js
@@ -1,30 +1,30 @@
 // const routes = ''; //(module.exports = require('next-routes')());
 
 // routes
-//   .add('main.index', '/') => index.js
+//   .add('main.index', '/')                                                                    => index.js
 //   .add('main.index-v2', '/v2')
 //   .add('publication.shortVersion', '/publications/:slug/shortversion')
 //   .add('publication.file', '/publications/:slug/downloadasset/:legacyid')
 //   .add('publication.entry', '/publications/:slug')
 //   .add('publication.print', '/publications/:slug/print')
 //   .add('publication.entry-v2', '/publication/:slug')
-//   .add('blog.index', '/blog') => /blog/index.js
-//   .add('blog.entry', '/blog/:slug') => /blog/[slug].js
+//   .add('blog.index', '/blog')                                                                => /blog/index.js
+//   .add('blog.entry', '/blog/:slug')                                                          => /blog/[slug].js
 //   .add('topic.article', '/topics/:slug/:topicPart')
-//   .add('topic.entry', '/topics/:slug') => /topics/[slug].js
-//   .add('topic.index', '/topics') => /topics/index.js
-//   .add('glossary.index', '/terms') => /terms/index.js
+//   .add('topic.entry', '/topics/:slug')                                                       => /topics/[slug].js
+//   .add('topic.index', '/topics')                                                             => /topics/index.js
+//   .add('glossary.index', '/terms')                                                           => /terms/index.js
 //   .add('keywords.index', '/keywords')
-//   .add('persons.index', '/the-team') => /the-team/index.js
-//   .add('persons.entry', '/the-team/:slug') => /the-team/[slug].js
+//   .add('persons.index', '/the-team')                                                         => /the-team/index.js
+//   .add('persons.entry', '/the-team/:slug')                                                   => /the-team/[slug].js
 //   .add('search-v2.index', '/search')
-//   .add('event.entry', '/events/:slug') => /events/[slug].js
-//   .add('course.entry', '/courses/:slug')
-//   .add('service.workshops', '/workshops-and-events') => /workshops-and-events/index.js
-//   .add('service.online-courses', '/online-courses') => /online-courses/index.js
-//   .add('service.helpdesk', '/helpdesk') => /helpdesk/index.js
-//   .add('service.about', '/about-u4') => /about-u4/index.js
+//   .add('event.entry', '/events/:slug')                                                       => /events/[slug].js
+//   .add('course.entry', '/courses/:slug')                                                     => /courses/[slug].js
+//   .add('service.workshops', '/workshops-and-events')                                         => /workshops-and-events/index.js
+//   .add('service.online-courses', '/online-courses')                                          => /online-courses/index.js
+//   .add('service.helpdesk', '/helpdesk')                                                      => /helpdesk/index.js
+//   .add('service.about', '/about-u4')                                                         => /about-u4/index.js
 //   .add('asset.entry', '/assets/:slug')
-//   .add('general.article', '/:slug') => [slug].js
+//   .add('general.article', '/:slug')                                                          => [slug].js
 //   .add('preview', '/preview/:type/:id')
 //   .add('publication.print.preview', '/printpreview/:id');
