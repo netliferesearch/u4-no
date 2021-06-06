d39fc8f57ce1c1a7d6c192b8eaaf8751940ea737
Nils Norman Haukås <mail@nilsnh.no>
2021-06-06 19:46:27 +0200

    tweak config to enable Webpack5

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,9 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({});
+if (process.env.ANALYZE === 'true') {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: true,
+  });
+  module.exports = withBundleAnalyzer({});
+  return;
+}
+
+module.exports = {};
