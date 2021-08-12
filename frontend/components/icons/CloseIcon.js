cafa7df94da13cdfe37be9fb41d4f5ffcf18f380
Dagne <dagne@asteroid.lt>
2021-08-12 10:42:06 +0300

    fix warning

--- frontend/components/icons/CloseIcon.js
+++ frontend/components/icons/CloseIcon.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function CloseIcon() {
   return (
     <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M18 1L1 18M1 1.00013L18 18" stroke="white" stroke-width="2" />
+      <path d="M18 1L1 18M1 1.00013L18 18" stroke="white" strokeWidth="2" />
     </svg>
   );
 }
