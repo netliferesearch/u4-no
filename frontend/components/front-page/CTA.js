b707ed3953e9def0485266a131cbe9fba09123d2
Robert Sjursen <robert.sjursen@cmi.no>
2022-04-29 10:51:38 +0200

    Use next/image on front page CTA

--- frontend/components/front-page/CTA.js
+++ frontend/components/front-page/CTA.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import sanityImageLoader from '../../helpers/sanityImageLoader';
 import { ArrowNext } from '../icons/ArrowNext';
 
 export const CTA = ({ img = '', data = {} }) => {
   return (
-    <div
-      className="c-cta"
-      style={{
-        backgroundImage: `linear-gradient(
-          90deg
-          , rgb(0 0 0 / 57%) 2.51%, rgb(11 11 11 / 54%) 25.95%, rgb(11 11 11 / 59%) 45.68%, rgba(255, 255, 255, 0) 99.64%),url(${img})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
-      }}
-    >
+    <div className="c-cta">
+      <Image 
+        loader={sanityImageLoader}
+        src={img}
+        layout="fill"
+        objectFit="cover"
+        priority="true"
+      />
       <div className="o-wrapper-medium c-cta__content">
         <h1 className="u-primary-heading u-text--white">{data.title}</h1>
         <p className="c-cta__paragraph">{data.text} </p>
