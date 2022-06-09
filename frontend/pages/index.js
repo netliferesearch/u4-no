e202fa42bba0d3ed4c659d6fc7c9b0cf0b00fbfd
Robert Sjursen <robert.sjursen@cmi.no>
2022-06-09 10:40:42 +0200

    use getStaticProps on frontpage and online courses page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DataLoader from '../helpers/data-loader';
+import { fetchAndMaterialize } from '../helpers/data-loader';
 import { Layout } from '../components/Layout';
 import Footer from '../components/general/footer/Footer';
 import PartnerAgencies from '../components/front-page/PartnerAgencies';
@@ -110,18 +110,32 @@ Frontpage.propTypes = {
   }).isRequired,
 };
 
-export default DataLoader(Frontpage, {
-  queryFunc: () => ({
-    sanityQuery: `{
-      "frontPage": *[(_type=="frontpage") && ((_id == "7d2f27a0-58b7-4ba0-8520-0e07bc879b04") || (slug.current == "frontpage-NEW"))][0]{id,title,sections,"imageUrl": featuredImage.asset->url,
-      "resources": resources[]->{_id,_type, "publicationType": publicationType->title, "articleTypeTitle": articleType[0]->title, title, date, standfirst, topics[]->{title}, "slug": slug.current,"titleColor": featuredImage.asset->metadata.palette.dominant.title, "imageUrl": featuredImage.asset->url, "pdfFile": pdfFile.asset->url}[0..4]},
-      "topics": *[_type == "topics"] | order(title) {_id, _type, title, longTitle, standfirst, slug, _updatedAt, "imageUrl": featuredImage.asset->url}[0..5],
-      "featured": {"publication": *[_type  == "publication" && language == "en_US"] | order(date.utc desc) {_id, _type, title, date, standfirst, "publicationType": publicationType->title, authors[]->{firstName, surname}, topics[]->{title, slug}, "imageUrl": featuredImage.asset->url, "slug": slug.current, "pdfFile": pdfFile.asset->url}[0..8], "blog": *[_type  == "blog-post" && references("daecef41-f87b-41ec-ad35-eefe31568ae0") && language == "en_US"] | order(date.utc desc) {_id, _type, title, date, standfirst, topics[]->{title}, "imageUrl": featuredImage.asset->url, "slug": slug.current}[0..1],},
-      "blogPosts": *[_type == "blog-post" && language == "en_US" && !references("daecef41-f87b-41ec-ad35-eefe31568ae0")] | order(date.utc desc) {_id, _type, title, date, standfirst, authors[]->{firstName, surname}, topics[]->{title}, "imageUrl": featuredImage.asset->url, "slug": slug.current}[0..8],
-      "events": *[_type in ["course", "event"] && defined(startDate)]{_type, title, startDate, lead, "slug": slug.current, topics[]->{title}, eventType,"future": select(dateTime(startDate.utc)-dateTime(now()) >=0 => 1,0),"timedistance": select(dateTime(startDate.utc)-dateTime(now()) >=0 => dateTime(startDate.utc)-dateTime(now()),dateTime(now())-dateTime(startDate.utc))}|order(future desc,timedistance asc)[0..2],
-      "institutions": *[_type == 'institution' && funder == true && !(_id in path "drafts.**")]
-      | order(name){_id, ${localize('name')}, funder, svgLogo, website},
-    }`,
-  }),
-  materializeDepth: 0,
+export default Frontpage;
+
+const queryFunc = () => ({
+  sanityQuery: `{
+    "frontPage": *[(_type=="frontpage") && ((_id == "7d2f27a0-58b7-4ba0-8520-0e07bc879b04") || (slug.current == "frontpage-NEW"))][0]{id,title,sections,"imageUrl": featuredImage.asset->url,
+    "resources": resources[]->{_id,_type, "publicationType": publicationType->title, "articleTypeTitle": articleType[0]->title, title, date, standfirst, topics[]->{title}, "slug": slug.current,"titleColor": featuredImage.asset->metadata.palette.dominant.title, "imageUrl": featuredImage.asset->url, "pdfFile": pdfFile.asset->url}[0..4]},
+    "topics": *[_type == "topics"] | order(title) {_id, _type, title, longTitle, standfirst, slug, _updatedAt, "imageUrl": featuredImage.asset->url}[0..5],
+    "featured": {"publication": *[_type  == "publication" && language == "en_US"] | order(date.utc desc) {_id, _type, title, date, standfirst, "publicationType": publicationType->title, authors[]->{firstName, surname}, topics[]->{title, slug}, "imageUrl": featuredImage.asset->url, "slug": slug.current, "pdfFile": pdfFile.asset->url}[0..8], "blog": *[_type  == "blog-post" && references("daecef41-f87b-41ec-ad35-eefe31568ae0") && language == "en_US"] | order(date.utc desc) {_id, _type, title, date, standfirst, topics[]->{title}, "imageUrl": featuredImage.asset->url, "slug": slug.current}[0..1],},
+    "blogPosts": *[_type == "blog-post" && language == "en_US" && !references("daecef41-f87b-41ec-ad35-eefe31568ae0")] | order(date.utc desc) {_id, _type, title, date, standfirst, authors[]->{firstName, surname}, topics[]->{title}, "imageUrl": featuredImage.asset->url, "slug": slug.current}[0..8],
+    "events": *[_type in ["course", "event"] && defined(startDate)]{_type, title, startDate, lead, "slug": slug.current, topics[]->{title}, eventType,"future": select(dateTime(startDate.utc)-dateTime(now()) >=0 => 1,0),"timedistance": select(dateTime(startDate.utc)-dateTime(now()) >=0 => dateTime(startDate.utc)-dateTime(now()),dateTime(now())-dateTime(startDate.utc))}|order(future desc,timedistance asc)[0..2],
+    "institutions": *[_type == 'institution' && funder == true && !(_id in path "drafts.**")]
+    | order(name){_id, ${localize('name')}, funder, svgLogo, website},
+  }`,
 });
+
+export const getStaticProps = async ctx => {
+  const { data, error = '' } = await fetchAndMaterialize({
+    nextContext: ctx,
+    queryFunc,
+    materializeDepth: 0,
+  });
+  if (error === 'No content found (dataLoader said this)') {
+    return { notFound: true };
+  }
+  return {
+    props: { data },
+    revalidate: 60,
+  };
+};
