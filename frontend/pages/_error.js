d291c944e019fb53d9d20b4ab516508c170bed6d
Robert Sjursen <robert.sjursen@cmi.no>
2022-08-31 13:37:45 +0200

    Move redirect handling from error handler to next.config.js

--- frontend/pages/_error.js
+++ frontend/pages/_error.js
@@ -6,37 +6,8 @@
 
 import React from 'react';
 import Error404 from '../components/Error404';
-import { redirectPermanent, getRedirect } from '../helpers/redirect';
 
 export default class ErrorHandler extends React.Component {
-  static async getInitialProps(ctx) {
-    if (ctx.res) {
-      const path = getRedirect(ctx);
-      if (path) return redirectPermanent(ctx, path);
-    }
-
-    /* TODO: make some query based on requested url to display suggested content
-    const client = sanityClient({
-      projectId: '1f1lcoov',
-      dataset: 'production',
-      token: '',
-      useCdn: false,
-    });
-
-    const queryFunc = () => ({
-      sanityQuery: '{ "suggestions": *[_type in ["publication"]] }',
-    });
-
-    const { sanityQuery, param } = queryFunc(ctx);
-    const sanityResults = await client.fetch(sanityQuery, param);
-
-    return { sanityResults };
-  */
-    if (ctx.res) {
-      ctx.res.statusCode = 404;
-    }
-    return { error: 'No matching content found', statusCode: 404 };
-  }
 
   render() {
     return <Error404 {...this.props} />;
