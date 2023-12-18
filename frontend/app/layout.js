2684391e4c4469567d50d8df7446a727f9669c2b
Robert Sjursen <robert.sjursen@cmi.no>
2023-12-18 08:36:32 +0100

    Activate GTM

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -7,9 +7,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <Script id="google-tag-manager" strategy="afterInteractive"> 
-                (false && {`
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KVW5J96')
-                `})
             </Script>
             <head>
                 <link
