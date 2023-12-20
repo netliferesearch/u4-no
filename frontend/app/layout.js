2bc1c425428116fccab2b7be1635f6afcda47bdd
Robert Sjursen <robert.sjursen@cmi.no>
2023-12-20 13:08:07 +0100

    Use next/font

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,16 +1,22 @@
 import { GoogleTagManager } from '@next/third-parties/google' 
+import { Lato } from 'next/font/google';
+
 import '../style/global.scss';
 import '../style/html.css';
 import Footer from 'components/general/footer/Footer';
 
+const lato = Lato({
+  weight: ['400', '700'], // 300 and 900 left out
+  style: ['normal'], // 'italic' left out
+  subsets: ['latin'],
+  variable: '--font-lato',
+  display: 'swap',
+})
+
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${lato.variable}`} >
             <head>
-                <link
-                    href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i,900&display=swap"
-                    rel="stylesheet"
-                />
                 <link rel="preconnect" href="https://cdn.sanity.io" />
             </head>
             <body>
