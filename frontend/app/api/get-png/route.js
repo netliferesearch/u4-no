b6a0bad0714b754db5c2e1ebc1594303149cc5db
Robert Sjursen <robert.sjursen@cmi.no>
2024-12-09 14:55:53 +0100

    Final style adjustments of Sanity Generate buttons

--- frontend/app/api/get-png/route.js
+++ frontend/app/api/get-png/route.js
@@ -1,20 +1,18 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-const debug = true;
-
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
   const assetId = searchParams.get('assetId');
   const actualUrl = `${process.env.THUMBNAIL_GENERATOR_URL}?assetId=${assetId}`;
-  debug && console.log('Fetching image from', actualUrl);
+  // console.log('Fetching image from', actualUrl);
   try {
     const response = await axios({
       url: actualUrl,
       method: 'GET',
       responseType: 'stream',
     });
-    debug && console.log('Response from /api/get-png:', response);
+    // console.log('Response from /api/get-png:', response);
     return new NextResponse(response.data, {
       headers: {
         'Content-Type': 'image/png',
