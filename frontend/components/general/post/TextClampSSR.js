b328050e4d34d0955c962bbaf26d29e8d26a1b1b
Robert Sjursen <robert.sjursen@cmi.no>
2025-01-17 16:40:14 +0100

    Use css line-clamp in post info

--- frontend/components/general/post/TextClampSSR.js
+++ frontend/components/general/post/TextClampSSR.js
@@ -1,33 +1,7 @@
-"use client";
-
-import React from 'react';
-import { useEffect, useState } from 'react';
-//import dynamic from 'next/dynamic';
-
-import TextClamp from 'react-string-clamp';
-//const TextClamp = dynamic(() =>
-//  import('react-string-clamp')
-//);
-
-const TextClampStatic = ({ text = '', lines = 1 }) => {
-  const maxChar = 50 * lines;
-  const str = text.toString();
-  return (str.length <= maxChar) 
-    ? (<div style={{display: 'block', width: '100%'}}>{str}</div>)
-    : (<div style={{display: 'block', width: '100%'}}>{str.substring(0, str.lastIndexOf(' ', maxChar)).concat(String.fromCharCode(8230))}</div>);
-}
-
-export const TextClampSSR = ({ text = '', lines = 1 }) => {
-  const [isClient, setIsClient] = useState(false);
-  useEffect(() => {
-    if ( typeof window !== 'undefined' ) {
-      setIsClient(true);
-    }
-  }, []);
-
-  //console.log(text,lines,typeof window);
-  //return <TextClampStatic text={text} lines={lines} />;
-  return ( isClient ) 
-    ? <TextClamp text={text} lines={lines} />
-    : <TextClampStatic text={text} lines={lines} />;
-}
+export const TextClampSSR = ({ text = '', lines = 3 }) => {
+  return (
+    <div className="c-text-clamp" style={{ WebkitLineClamp: lines }}>
+      {text}
+    </div>
+  );
+};
